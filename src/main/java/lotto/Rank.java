8b2b92a17ba50adeb84ef1696ccc410eab58c8c1
baker.gus <baker.gus@kakaocorp.com>
2023-01-02 16:43:39 +0900

    feat: Rank에 prize 필드 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,18 +3,20 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(4, new LotteryMatch(6,false)),
-    SECOND(3, new LotteryMatch(5,true)),
-    THIRD(2, new LotteryMatch(5,false)),
-    FOURTH(1, new LotteryMatch(4,false)),
-    FIFTH(0, new LotteryMatch(3,false));
+    FIRST(4, new LotteryMatch(6,false), 2000000000),
+    SECOND(3, new LotteryMatch(5,true),30000000),
+    THIRD(2, new LotteryMatch(5,false),1500000),
+    FOURTH(1, new LotteryMatch(4,false), 50000),
+    FIFTH(0, new LotteryMatch(3,false), 5000);
 
     public final int index;
     public final LotteryMatch match;
+    public final int prize;
 
-    Rank(int index, LotteryMatch match) {
+    Rank(int index, LotteryMatch match, int prize) {
         this.index = index;
         this.match = match;
+        this.prize = prize;
     }
 
     public static Rank getRank(LotteryMatch match){
