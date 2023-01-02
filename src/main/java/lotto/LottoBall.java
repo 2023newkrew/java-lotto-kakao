6cde0511ff49fd817860310d8494042dfffb3eb8
gh1224 <rlarkgus0@naver.com>
2023-01-02 14:15:33 +0900

    feat: 로또볼 생성 및 값 범위 체크

---
+++ src/main/java/lotto/LottoBall.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoBall {
+    private final int ball;
+    public LottoBall(int ball) {
+        if( ball <= 0 || ball > 45){
+            throw new IllegalArgumentException("ball 은 1 ~ 45 사이의 숫자여야 합니다.");
+        }
+        this.ball = ball;
+    }
+
+    @Override
+    public String toString() {
+        return "LottoBall{" +
+                "ball=" + ball +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoBall lottoBall = (LottoBall) o;
+        return ball == lottoBall.ball;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ball);
+    }
+}
