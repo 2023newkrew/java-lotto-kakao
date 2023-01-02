ed7f4ee26a702ca442d1e58cc7d2edc5a9bd8f52
Eithea <oguraff94@gmail.com>
2023-01-02 17:10:03 +0900

    chore(Lotto): fix lint

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,14 +1,13 @@
 package lotto.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
 
     private final List<Integer> numbers;
 
-    public Lotto() {
-        this.numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 45));
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
