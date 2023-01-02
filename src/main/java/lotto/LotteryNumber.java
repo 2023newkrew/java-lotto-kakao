c4ef5061a5ddef0e36dc9728bf53e66aa6174aa2
baker.gus <baker.gus@kakaocorp.com>
2023-01-02 18:55:13 +0900

    feat: LotteryNumber에 number 검증 로직 추가

--- src/main/java/lotto/LotteryNumber.java
+++ src/main/java/lotto/LotteryNumber.java
@@ -5,13 +5,23 @@ import java.util.List;
 
 public class LotteryNumber {
     private final List<Integer> numbers;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public LotteryNumber(List<Integer> numbers){
+        for(final Integer number : numbers) validateNumber(number);
+
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new RuntimeException("잘못 된 로또 번호");
+        }
     }
 
     @Override
