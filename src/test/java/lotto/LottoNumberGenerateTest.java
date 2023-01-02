c55de3039d919ee5e294354cff83933b871cf61c
abcworld123 <rkaxhdals@gmail.com>
2023-01-02 16:38:27 +0900

    test: 로또 번호 생성 테스트
    - `Generator` 인터페이스 생성
    - 익명 함수를 통한 랜덤 값 주입
    - 서로 다른 숫자 생성 테스트
    - 바운더리 테스트

--- src/test/java/lotto/LottoNumberGenerateTest.java
+++ src/test/java/lotto/LottoNumberGenerateTest.java
@@ -1,25 +1,35 @@
 package lotto;
 
-import lotto.domain.Lotto;
+import lotto.util.Generator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class LottoTest {
-
+class LottoNumberGenerateTest {
     @Test
     @DisplayName("로또는 6개의 숫자로 이루어져 있다.")
     void lottoHas6Numbers() {
-        Lotto lotto = new Lotto();
-        assertThat(lotto.getNumbers().size()).isEqualTo(6);
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(generator.getNumbers()).hasSize(6);
     }
 
     @Test
     @DisplayName("로또 번호는 서로 다른 숫자다.")
     void lottoNumberNotDuplicate() {
+        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(generator.getNumbers().stream().distinct().count()).isEqualTo(6);
+    }
 
+    @Test
+    @DisplayName("로또 번호는 1과 45 사이의 숫자로만 이루어져 있다.")
+    void lottoNumberin1To45() {
+        Generator generator = () -> Arrays.asList(1, 5, 15 ,28, 39, 45);
+        assertThat(generator.getNumbers().stream()
+                .filter(number -> 1 <= number && number <= 45)
+                .count()
+        ).isEqualTo(6);
     }
 }
