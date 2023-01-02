34e03e57fb5f6c5ae664e479f0a959adf8849e90
orion.jeong <orion.jeong@kakaocorp.com>
2023-01-02 15:55:59 +0900

    test: 로또 여러장 발권하는 테스트 작성

--- src/test/java/lotto/IssuerTest.java
+++ src/test/java/lotto/IssuerTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.model.Issuer;
 import lotto.model.Lotto;
+import lotto.model.LottoList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,14 +16,16 @@ public class IssuerTest {
     @Test
     @DisplayName("로또 길이 테스트")
     void lotto_length_test() {
-        Lotto lotto = Issuer.issue();
+        LottoList lottoList = Issuer.issue(1);
+        Lotto lotto = lottoList.get(0);
         assertEquals(lotto.length(), LottoSettings.MAX_LENGTH.getValue());
     }
 
     @Test
     @DisplayName("로또 숫자 범위 테스트")
     void lotto_range_test() {
-        Lotto lotto = Issuer.issue();
+        LottoList lottoList = Issuer.issue(1);
+        Lotto lotto = lottoList.get(0);
         for (int index = 0; index < lotto.length(); index++) {
             assertTrue(lotto.getIthNumber(index) <= LottoSettings.MAX_RANGE.getValue() &&
                     lotto.getIthNumber(index) >= LottoSettings.MIN_RANGE.getValue());
@@ -32,7 +35,15 @@ public class IssuerTest {
     @Test
     @DisplayName("로또 숫자 중복 테스트")
     void lotto_duplicate_test() {
-        Lotto lotto = Issuer.issue();
+        LottoList lottoList = Issuer.issue(1);
+        Lotto lotto = lottoList.get(0);
         assertEquals(new HashSet<>(lotto.getNumbers()).size(), lotto.length());
+    }
+
+    @Test
+    @DisplayName("발권 매수 테스트")
+    void lotto_list_size_test() {
+        LottoList lottoList = Issuer.issue(14);
+        assertEquals(lottoList.length(), 14);
     }
 }
