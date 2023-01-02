46c84d353e90a4036a17341bbac544f39c470b32
정핸모 <gusah009@naver.com>
2023-01-02 17:04:58 +0900

    feat: 구입한 로또 개수를 출력해주는 기능 추가

--- src/main/java/javalotto/view/OutputView.java
+++ src/main/java/javalotto/view/OutputView.java
@@ -1,4 +1,11 @@
 package javalotto.view;
 
+
+import javalotto.domain.LottoCount;
+
 public class OutputView {
+
+    public void printLottoCount(LottoCount lottoCount) {
+        System.out.println(lottoCount.getCount() + "개를 구매했습니다.");
+    }
 }
