f535a818063823394814231e304e569f9c8bb9c0
orion.jeong <orion.jeong@kakaocorp.com>
2023-01-02 17:40:19 +0900

    feat: 당첨 번호와 로또 한 장의 번호 비교 기능 작성

---
+++ src/main/java/lotto/model/MatchedResult.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import java.util.Objects;
+
+public class MatchedResult {
+
+    private final Integer matchedCount;
+
+    private final Boolean isBonusNumberMatched;
+
+    public MatchedResult(Integer matchedCount, Boolean isBonusNumberMatched) {
+        this.matchedCount = matchedCount;
+        this.isBonusNumberMatched = isBonusNumberMatched;
+    }
+
+    public Integer getMatchedCount() {
+        return matchedCount;
+    }
+
+    public Boolean getBonusNumberMatched() {
+        return isBonusNumberMatched;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MatchedResult that = (MatchedResult) o;
+
+        if (!Objects.equals(matchedCount, that.matchedCount)) return false;
+        return Objects.equals(isBonusNumberMatched, that.isBonusNumberMatched);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = matchedCount != null ? matchedCount.hashCode() : 0;
+        result = 31 * result + (isBonusNumberMatched != null ? isBonusNumberMatched.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,61 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class WinningNumbers {
+
+    private final MainNumbers mainNumbers;
+
+    private final Integer bonusNumber;
+
+    public WinningNumbers(MainNumbers mainNumbers, Integer bonusNumber) {
+        this.mainNumbers = mainNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public MatchedResult check(Lotto lotto) {
+        return new MatchedResult(checkMainNumbers(lotto), checkBonusNumber(lotto));
+    }
+
+    private Integer checkMainNumbers(Lotto lotto) {
+        Set<Integer> standard = new TreeSet<>(mainNumbers.numbers);
+        standard.retainAll(new TreeSet<>(lotto.getNumbers()));
+        return standard.size();
+    }
+
+    private Boolean checkBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+
+    public static class MainNumbers {
+
+        private final List<Integer> numbers;
+
+        public MainNumbers(List<Integer> numbers) {
+            this.numbers = numbers;
+            validateNumbers();
+        }
+
+        private void validateNumbers() {
+            boolean result = numbers.stream()
+                    .filter(number -> checkRange(number) && checkLength())
+                    .collect(Collectors.toSet())
+                    .size() == numbers.size();
+            if (!result) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private Boolean checkRange(Integer number) {
+            return number >= LottoSettings.MIN_RANGE.getValue() &&
+                    number <= LottoSettings.MAX_RANGE.getValue();
+        }
+
+        private Boolean checkLength() {
+            return numbers.size() == LottoSettings.MAX_LENGTH.getValue();
+        }
+    }
+}
