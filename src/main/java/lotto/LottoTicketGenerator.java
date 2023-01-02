0d5c95fe8a3a489576f173b781d23896a4aaac98
gh1224 <rlarkgus0@naver.com>
2023-01-02 16:12:29 +0900

    feat: 랜덤 로또 번호 생성

---
+++ src/main/java/lotto/LottoTicketGenerator.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicketGenerator {
+
+    LottoTicket generate(LottoBallGenerator generator){
+        List<LottoBall> lottoNumbers = new ArrayList<>();
+        while (lottoNumbers.size() != 6) {
+            lottoNumbers.add(generateEachBall(lottoNumbers, generator));
+        }
+        return new LottoTicket(lottoNumbers);
+    }
+
+    private LottoBall generateEachBall(List<LottoBall> lottoNumbers, LottoBallGenerator generator) {
+        LottoBall generatedBall = generator.generateBall();
+        for (; lottoNumbers.contains(generatedBall); generatedBall = generator.generateBall()) {}
+        return generatedBall;
+    }
+
+    @FunctionalInterface
+    public interface LottoBallGenerator{
+        LottoBall generateBall();
+    }
+}
