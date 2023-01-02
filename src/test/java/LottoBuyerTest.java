77580c8cce90cdcd701902238ebf7b4b1379443c
Subin Min <tnqls5417@naver.com>
2023-01-02 18:58:23 +0900

    test: LottoBuyer 테스트
    - 로또 리스트를 받아 당첨된 등수 개수를 확인
    - 로또 리스트를 받아 수익률을 계산

---
+++ src/test/java/LottoBuyerTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoBuyerTest {
+
+    @Test
+    void 로또_당첨_등수를_확인한다() {
+        int price = 3500;
+        List<Lotto> lottos = List.of(
+                Lotto.ofNumbers(List.of(1,2,3,4,5,6)),
+                Lotto.ofNumbers(List.of(3,4,5,6,7,8)),
+                Lotto.ofNumbers(List.of(5,6,7,8,9,10))
+        );
+        LottoBuyer lottoBuyer = new LottoBuyer(price, (money)->lottos);
+
+        List<LottoNumber> winningNumbers = IntStream
+                .range(1, 7)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+        LottoNumber bonusNumber = new LottoNumber(45);
+
+        LottoResult lottoResult = lottoBuyer.check(winningNumbers, bonusNumber);
+        assertThat(lottoResult.get(LottoStatus.FIRST_PLACE)).isEqualTo(1);
+        assertThat(lottoResult.get(LottoStatus.SECOND_PLACE)).isEqualTo(0);
+        assertThat(lottoResult.get(LottoStatus.THIRD_PLACE)).isEqualTo(0);
+        assertThat(lottoResult.get(LottoStatus.FOURTH_PLACE)).isEqualTo(1);
+        assertThat(lottoResult.get(LottoStatus.FIFTH_PLACE)).isEqualTo(0);
+    }
+
+    @Test
+    void 로또_수익률을_계산한다() {
+        int price = 3500;
+        List<Lotto> lottos = List.of(
+                Lotto.ofNumbers(List.of(1,2,3,4,5,6)),
+                Lotto.ofNumbers(List.of(3,4,5,6,7,8)),
+                Lotto.ofNumbers(List.of(5,6,7,8,9,10))
+        );
+        List<LottoNumber> winningNumbers = IntStream
+                .range(1, 7)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+        LottoNumber bonusNumber = new LottoNumber(45);
+
+        LottoBuyer lottoBuyer = new LottoBuyer(price, (money)->lottos);
+
+        lottoBuyer.check(winningNumbers, bonusNumber);
+        assertThat(lottoBuyer.calculateEarningRate()).isEqualTo(666683.33);
+    }
+}
