9a5d4fc004b43c7c960a781e0911e7aa6cc67c63
Subin Min <tnqls5417@naver.com>
2023-01-02 15:41:15 +0900

    feat: LottoNumber 생성 기능 추가
    - 1~45 사이 숫자가 아닐 경우 예외 발생

---
+++ src/main/java/LottoNumber.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class LottoNumber {
+    private Integer number;
+
+    private static final Integer LOTTO_NUMBER_START = 1;
+    private static final Integer LOTTO_NUMBER_END = 45;
+
+    public LottoNumber(Integer number) {
+        validateLottoNumber(number);
+        this.number = number;
+    }
+
+    private void validateLottoNumber(Integer number) {
+        if (number < LOTTO_NUMBER_START || number > LOTTO_NUMBER_END) {
+            throw new IllegalArgumentException("로또 숫자는 1 이상 45 이하여야합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
