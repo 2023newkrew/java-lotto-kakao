1940f312a6a7e266e3913abb7383b1c16aca9148
siryeong <siryeongchoi@gmail.com>
2023-01-02 17:55:06 +0900

    test(lotto number): 불필요한 `Generate`사용 수정
    로또 넘버 테스트 중 불필요한 generate 사용을 -> List.of 로 수정함

--- src/test/java/lotto/LottoNumberGenerateTest.java
+++ src/test/java/lotto/LottoNumberGenerateTest.java
@@ -1,35 +1,34 @@
 package lotto;
 
-import lotto.util.Generator;
+import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoNumberGenerateTest {
     @Test
     @DisplayName("로또는 6개의 숫자로 이루어져 있다.")
     void lottoHas6Numbers() {
-        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
-        assertThat(generator.getNumbers()).hasSize(6);
+        assertThat(List.of(1,2,3,4,5,6)).hasSize(6);
     }
 
     @Test
     @DisplayName("로또 번호는 서로 다른 숫자다.")
     void lottoNumberNotDuplicate() {
-        Generator generator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
-        assertThat(generator.getNumbers().stream().distinct().count()).isEqualTo(6);
+        assertThat(
+                List.of(1,2,3,4,5,6).stream().distinct().count()
+        ).isEqualTo(6);
     }
 
     @Test
     @DisplayName("로또 번호는 1과 45 사이의 숫자로만 이루어져 있다.")
     void lottoNumberin1To45() {
-        Generator generator = () -> Arrays.asList(1, 5, 15 ,28, 39, 45);
-        assertThat(generator.getNumbers().stream()
-                .filter(number -> 1 <= number && number <= 45)
-                .count()
+        assertThat(
+                List.of(1,2,3,4,5,6).stream().filter(number -> 1 <= number && number <= 45).count()
         ).isEqualTo(6);
     }
 }
