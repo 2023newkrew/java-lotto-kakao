945a7786f7caae5babbe7a6f12dba46dfe9e3db0
Eithea <oguraff94@gmail.com>
2023-01-02 18:50:47 +0900

    feat(Lotto): enum Price, class PriceResult 구현
    Price : 각 당첨의 등수와 금액
    PriceResult : Lottos의 당첨 결과 및 상금 계산

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -1,10 +1,26 @@
 package lotto.model;
 
 public enum Price {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    NOTHING
+    FIRST("6개 일치",2000000000),
+    SECOND("5개 일치, 보너스 볼 일치",30000000),
+    THIRD("5개 일치", 1500000),
+    FOURTH("4개 일치", 50000),
+    FIFTH("3개 일치", 5000),
+    NOTHING("", 0);
+
+    private final String matchDescription;
+    private final long price;
+
+    Price(String matchDescription, long price) {
+        this.matchDescription = matchDescription;
+        this.price = price;
+    }
+
+    public String matchDescription(){
+        return matchDescription;
+    }
+
+    public long price(){
+        return price;
+    }
 }
