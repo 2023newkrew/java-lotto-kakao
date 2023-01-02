e9d98e23bd4813e26d39470adf0bddd782dbb52c
fbgnwhife1 <fbgnwhife@gamil.com>
2023-01-02 16:15:01 +0900

    feat: 로또 용지를 생성하는 기능, 유효성 검사

---
+++ src/main/java/LottoTicket.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LottoTicket {
+
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    private final List<Integer> lottoNumbers = new ArrayList<>();
+
+    public LottoTicket(List<Integer> lottoNumbers) {
+        //숫자 중복 체크
+        validateNumberDuplication(lottoNumbers);
+        validateNumberRange(lottoNumbers);
+        Collections.sort(lottoNumbers);
+        this.lottoNumbers.addAll(lottoNumbers);
+    }
+
+    private void validateNumberDuplication(List<Integer> lottoNumbers) {
+        HashSet<Integer> hs = new HashSet<>(lottoNumbers);
+        if (hs.size() != LOTTO_NUMBER_SIZE) throw new IllegalArgumentException();
+    }
+
+    private void validateNumberRange(List<Integer> lottoNumbers) {
+        lottoNumbers
+                .forEach((number) -> {
+                    if (number < 1 || number > 45) throw new IllegalArgumentException();
+                });
+    }
+
+
+}
