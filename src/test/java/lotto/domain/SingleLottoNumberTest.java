3a01833d11e5a15141cd0d6991e6d2521e0328b2
HQkim <hyunkyu001@gmail.com>
2023-01-02 17:44:34 +0900

    test: SingleLottoNumberTest에 테스트 추가
    - 로또 번호는 1에서 45 사이여야 한다
    - 로또 번호는 1에서 45 사이가 아니면 예외가 발생한다

---
+++ src/test/java/lotto/domain/SingleLottoNumberTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SingleLottoNumberTest {
+
+    @RepeatedTest(45)
+    void 로또_번호는_1_에서_45_사이여야_한다(RepetitionInfo repetitionInfo) {
+        int singleLottoNumber = repetitionInfo.getCurrentRepetition();
+
+        assertThatCode(() -> new SingleLottoNumber(singleLottoNumber)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10, 0, 46, 100})
+    void 로또_번호는_1_에서_45_사이가_아니면_예외가_발생한다(int singleLottoNumber) {
+        assertThatThrownBy(() -> new SingleLottoNumber(singleLottoNumber)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
