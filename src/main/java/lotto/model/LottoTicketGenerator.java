8047bb79c4fefaeb20953de90e49bb7685bd0a09
jay.den <jay.den@kakaocorp.com>
2023-01-02 16:43:40 +0900

    refactor : LottoTicketGenerator 수정
    - 싱글턴 패턴 적용

--- src/main/java/lotto/model/LottoTicketGenerator.java
+++ src/main/java/lotto/model/LottoTicketGenerator.java
@@ -5,12 +5,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoTicketGenerator {
-    public LottoTicket generate() {
-        List<LottoValue> nums = new ArrayList<>();
-        for (int i = 1 ; i <= 45 ; i++){
-            nums.add(new LottoValue(i));
+    private final List<LottoValue> lottoDomain;
+    private static LottoTicketGenerator instance;
+
+    private LottoTicketGenerator() {
+        lottoDomain = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            lottoDomain.add(new LottoValue(i));
         }
-        Collections.shuffle(nums);
-        return new LottoTicket(nums.subList(0, 6));
+    }
+
+    public static LottoTicketGenerator getInstance() {
+        if (instance == null) {
+            instance = new LottoTicketGenerator();
+        }
+        return instance;
+    }
+
+    public LottoTicket generate() {
+        Collections.shuffle(lottoDomain);
+        return new LottoTicket(lottoDomain.subList(0, LottoTicket.VALUES_COUNT));
     }
 }
--- src/test/java/lotto/model/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/model/LottoTicketGeneratorTest.java
@@ -7,6 +7,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 class LottoTicketGeneratorTest {
     @Test
     void 난수_값_6개_생성(){
-        assertThatCode(() -> new LottoTicketGenerator().generate()).doesNotThrowAnyException();
+        assertThatCode(() -> LottoTicketGenerator.getInstance().generate()).doesNotThrowAnyException();
     }
 }
