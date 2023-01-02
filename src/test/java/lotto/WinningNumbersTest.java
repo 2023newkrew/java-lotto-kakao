8cee62c9d3418f318afbfd2618b78045692c473e
orion.jeong <orion.jeong@kakaocorp.com>
2023-01-02 17:39:34 +0900

    test: 당첨 번호와 로또 한 장의 번호 비교 테스트 작성

---
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import lotto.model.Lotto;
+import lotto.model.MatchedResult;
+import lotto.model.WinningNumbers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WinningNumbersTest {
+
+    private WinningNumbers winningNumbers;
+
+    @BeforeEach
+    void setUp() {
+        winningNumbers = new WinningNumbers(
+                new WinningNumbers.MainNumbers(List.of(1, 3, 5, 7, 9, 11)),
+                27
+        );
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 로또 번호 비교 결과")
+    void matched_result_test() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        MatchedResult matchedResult = winningNumbers.check(lotto);
+        assertEquals(matchedResult, new MatchedResult(
+                3, false
+        ));
+    }
+}
