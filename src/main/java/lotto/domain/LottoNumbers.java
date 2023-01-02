569cc8f995f0733e69d280d417f7652457d2e26e
HQkim <hyunkyu001@gmail.com>
2023-01-02 17:59:15 +0900

    feat: LottoNumbers 유효성 검사 추가
    - "중복된 로또 번호가 있으면 예외가 발생한다" 유효성 검사 추가
    - validateLottoNumbers()로 메서드 추출

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -7,10 +7,19 @@ public class LottoNumbers {
     private final List<SingleLottoNumber> singleLottoNumbers;
 
     public LottoNumbers(List<SingleLottoNumber> singleLottoNumbers) {
+        validateLottoNumbers(singleLottoNumbers);
+
+        this.singleLottoNumbers = singleLottoNumbers;
+    }
+
+    private void validateLottoNumbers(List<SingleLottoNumber> singleLottoNumbers) {
         if (singleLottoNumbers.size() != LOTTO_NUMBER_LENGTH) {
             throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
         }
 
-        this.singleLottoNumbers = singleLottoNumbers;
+        long uniqueNumberSize = singleLottoNumbers.stream().distinct().count();
+        if (singleLottoNumbers.size() != uniqueNumberSize) {
+            throw new IllegalArgumentException("중복된 로또 번호가 있으면 안됩니다.");
+        }
     }
 }
