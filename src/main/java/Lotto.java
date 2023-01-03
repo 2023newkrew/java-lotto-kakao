b27fe025da8da24f61ed9e38b441c7acf20755bc
Subin Min <tnqls5417@naver.com>
2023-01-03 12:47:30 +0900

    feat: Lotto 검증 구현
    - LottoBuyer.check() : List<LottoNumber> -> Lotto
    - Lotto.ofNumbers() : 정수 리스트로 생성시 검증 추가

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,7 +1,10 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final Integer LOTTO_SIZE = 6;
     private final List<LottoNumber> numbers;
 
     Lotto() {
@@ -15,16 +18,27 @@ public class Lotto {
                 .collect(Collectors.toList());
     }
 
-    public LottoStatus getPlace(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+    public LottoStatus getPlace(Lotto winningNumbers, LottoNumber bonusNumber) {
         Integer correctWinningNumberCount = 0;
         for(LottoNumber number : numbers) {
-            if (winningNumbers.contains(number))
+            if (winningNumbers.numbers.contains(number))
                 correctWinningNumberCount++;
         }
         return LottoStatus.getStatus(correctWinningNumberCount, numbers.contains(bonusNumber));
     }
 
     static public Lotto ofNumbers(List<Integer> numbers) {
+        validateLottoNumbers(numbers);
         return new Lotto(numbers);
+    }
+
+    static private void validateLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 숫자는 6개이어야 합니다.");
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 숫자는 중복이 불가합니다.");
+        }
     }
 }
--- src/main/java/LottoBuyer.java
+++ src/main/java/LottoBuyer.java
@@ -1,13 +1,9 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LottoBuyer {
     private Integer money;
     private List<Lotto> lottos;
-
     private LottoResult lottoResult;
 
     LottoBuyer(Integer money, LottoObtainPlace place) {
@@ -15,12 +11,12 @@ public class LottoBuyer {
         this.lottos = place.buy(money);
     }
 
-    public LottoResult check(List<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
+    public LottoResult check(Lotto winningNumbers, LottoNumber bonusNumber) {
         List<LottoStatus> statuses = lottos.stream()
                 .map(lotto -> lotto.getPlace(winningNumbers, bonusNumber))
                 .collect(Collectors.toList());
 
-        Map<LottoStatus, Integer> map = new HashMap<>();
+        Map<LottoStatus, Integer> map = new EnumMap<>(LottoStatus.class);
         for (LottoStatus lottoStatus : LottoStatus.values()) {
             map.put(lottoStatus, Collections.frequency(statuses, lottoStatus));
         }
@@ -37,7 +33,6 @@ public class LottoBuyer {
         if (earningMoney == 0L) {
             return 0.00d;
         }
-        System.out.println(earningMoney);
         return Math.floor(earningMoney / (double) investMoney * 100) / 100;
     }
 
