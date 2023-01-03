ede485c8b4f4ca00557e3941a7aab07f418047cf
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 17:13:20 +0900

    test: 수익률 계산 테스트 작성

--- src/test/java/domain/TotalResultTest.java
+++ src/test/java/domain/TotalResultTest.java
@@ -1,18 +1,30 @@
 package domain;
 
 import common.state.Result;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
 
 class TotalResultTest {
 
     @Test
-    void put() {
+    void increaseValueOfResultTest() {
         TotalResult totalResult = new TotalResult();
         int previousThree =totalResult.getValueOfResult(Result.THREE);
         totalResult.increaseValueOfResult(Result.THREE);
         totalResult.increaseValueOfResult(Result.THREE);
         int currentThree =totalResult.getValueOfResult(Result.THREE);
         assertThat(currentThree).isEqualTo(previousThree + 2);
+    }
+
+    @Test
+    void getProfitTest() {
+        Map<Result, Integer> seedTotalResult = Map.of(Result.THREE, 1);
+        TotalResult totalResult = new TotalResult(seedTotalResult);
+        assertThat(totalResult.getProfit()).isEqualTo(0.35);
     }
 }
