d9cc1cf6475e14c6dcad6d8881fe81f720c382bd
정핸모 <gusah009@naver.com>
2023-01-03 11:26:02 +0900

    feat: 구입한 모든 로또의 등수를 출력해주는 기능 추가
    - Lotto 당첨 결과를 알려주는 LottoResult 클래스 추가

--- src/main/java/javalotto/domain/Lottos.java
+++ src/main/java/javalotto/domain/Lottos.java
@@ -1,10 +1,13 @@
 package javalotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Lottos {
-    List<Lotto> lottos;
+    private final List<Lotto> lottos;
 
     private Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
@@ -16,6 +19,26 @@ public class Lottos {
 
     public int size() {
         return lottos.size();
+    }
+
+    public LottoResult getLottoResult(WinningLotto winningLotto) {
+        Map<Rank, Integer> rankCount = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankCount.put(rank, 0);
+        }
+        setRankCountBy(winningLotto, rankCount);
+        return LottoResult.of(rankCount);
+    }
+
+    private void setRankCountBy(WinningLotto winningLotto, Map<Rank, Integer> rankCount) {
+        lottos.stream()
+                .map(winningLotto::getRank)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .forEach(rank -> {
+                    int count = rankCount.get(rank);
+                    rankCount.replace(rank, count + 1);
+                });
     }
 
     @Override
