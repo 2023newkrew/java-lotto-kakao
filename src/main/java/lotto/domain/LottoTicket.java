ab0158f75002144f317f97a1c874071c7f877fcb
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-03 11:35:03 +0900

    feat(lotto): 당첨 번호가 아닌 경우 예외를 발생하게 구현

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -3,16 +3,21 @@ package lotto.domain;
 import java.util.ArrayList;
 
 public class LottoTicket {
-
+    private static final int LOTTO_TICKET_SIZE = 6;
     private final ArrayList<Integer> lottoNumbers;
 
     public LottoTicket(ArrayList<Integer> lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
+        lottoNumberCountCheck();
     }
 
     public ArrayList<Integer> getLottoNumbers() {
         return this.lottoNumbers;
     }
 
-
+    private void lottoNumberCountCheck() {
+        if (lottoNumbers.size() != LOTTO_TICKET_SIZE) {
+            throw new IllegalArgumentException("로또 번호의 개수가 6개가 아닙니다");
+        }
+    }
 }
