f40636ac9c5f1b2ac8a4394ec7f2622d5496a2db
HQkim <hyunkyu001@gmail.com>
2023-01-03 09:36:26 +0900

    test: LottoTest 생성 및 테스트 구현
    - 정답과 보너스 볼이 있어야 한다
    - 정답과 6개가 모두 일치하면 1등상을 받아야 한다
    - 정답과 5개가 일치하고 보너스 숫자와 일치하면 2등상을 받아야 한다
    - 정답과 5개가 일치하면 3등상을 받아야 한다
    - 정답과 4개가 일치하면 4등상을 받아야 한다
    - 정답과 3개가 일치하면 5등상을 받아야 한다
    - 정답과 3개 미만이 일치하면 아무것도 받지 못한다

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,125 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class LottoTest {
+    LottoNumbers answerlottoNumbers = new LottoNumbers(
+            Arrays.asList(
+                    new SingleLottoNumber(1),
+                    new SingleLottoNumber(2),
+                    new SingleLottoNumber(3),
+                    new SingleLottoNumber(4),
+                    new SingleLottoNumber(5),
+                    new SingleLottoNumber(6)
+            )
+    );
+    SingleLottoNumber bonusNumber = new SingleLottoNumber(7);
+    Lotto answerLotto = new Lotto(answerlottoNumbers, bonusNumber);
+
+    @Test
+    void 정답과_보너스_볼이_있어야_한다() {
+        assertThatCode(() -> new Lotto(answerlottoNumbers, bonusNumber)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 정답과_6개가_모두_일치하면_1등상을_받아야_한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(1),
+                new SingleLottoNumber(2),
+                new SingleLottoNumber(3),
+                new SingleLottoNumber(4),
+                new SingleLottoNumber(5),
+                new SingleLottoNumber(6)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.FIRST_PRIZE);
+    }
+
+    @Test
+    void 정답과_5개가_일치하고_보너스_숫자와_일치하면_2등상을_받아야_한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(1),
+                new SingleLottoNumber(2),
+                new SingleLottoNumber(3),
+                new SingleLottoNumber(4),
+                new SingleLottoNumber(5),
+                new SingleLottoNumber(7)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.SECOND_PRIZE);
+    }
+
+    @Test
+    void 정답과_5개가_일치하면_3등상을_받아야_한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(1),
+                new SingleLottoNumber(2),
+                new SingleLottoNumber(3),
+                new SingleLottoNumber(4),
+                new SingleLottoNumber(5),
+                new SingleLottoNumber(45)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.THIRD_PRIZE);
+    }
+
+    @Test
+    void 정답과_4개가_일치하면_4등상을_받아야_한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(1),
+                new SingleLottoNumber(2),
+                new SingleLottoNumber(3),
+                new SingleLottoNumber(4),
+                new SingleLottoNumber(44),
+                new SingleLottoNumber(45)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.FOURTH_PRIZE);
+    }
+
+    @Test
+    void 정답과_3개가_일치하면_5등상을_받아야_한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(1),
+                new SingleLottoNumber(2),
+                new SingleLottoNumber(3),
+                new SingleLottoNumber(43),
+                new SingleLottoNumber(44),
+                new SingleLottoNumber(45)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.FIFTH_PRIZE);
+    }
+
+    @Test
+    void 정답과_3개_미만이_일치하면_아무것도_받지_못한다() {
+        List<SingleLottoNumber> singleLottoNumbers = Arrays.asList(
+                new SingleLottoNumber(40),
+                new SingleLottoNumber(41),
+                new SingleLottoNumber(42),
+                new SingleLottoNumber(43),
+                new SingleLottoNumber(44),
+                new SingleLottoNumber(45)
+        );
+
+        LottoNumbers userLotto = new LottoNumbers(singleLottoNumbers);
+
+        assertThat(answerLotto.getPrize(userLotto)).isEqualTo(LottoPrize.NONE);
+    }
+}
