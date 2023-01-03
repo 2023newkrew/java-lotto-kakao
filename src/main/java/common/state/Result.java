2c6568d7a538d417ebe7eea4f06a07d51803aca7
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 17:14:50 +0900

    refac: 당첨 결과 enum에 당첨금 값 추가

--- src/main/java/common/state/Result.java
+++ src/main/java/common/state/Result.java
@@ -2,6 +2,12 @@ package common.state;
 
 public enum Result {
 
-    NONE, THREE, FOUR, FIVE, FIVEBONUS, SIX;
+    NONE(0), THREE(5_000), FOUR(50_000), FIVE(1500_000), FIVEBONUS(30_000_000), SIX(2_000_000_000);
+
+    private int winnings;
+
+    private Result(int winnings) {
+        this.winnings = winnings;
+    }
 
 }
