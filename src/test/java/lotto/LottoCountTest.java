eeb6644ae4c93658881d2e83176035dfa486224c
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-03 15:27:07 +0900

    feat : LottoCountTest 테스트 개인 코드 컨벤션 최적화

--- src/test/java/lotto/LottoCountTest.java
+++ src/test/java/lotto/LottoCountTest.java
@@ -9,16 +9,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LottoCountTest {
     @ParameterizedTest
-    @ValueSource(ints={-1,-100,-3})
-    void 음수가_되면_안됨(int val){
-        assertThatThrownBy(()->{
+    @ValueSource(ints = {-1, -100, -3})
+    void 음수가_되면_안됨(int val) {
+        assertThatThrownBy(() -> {
             LottoCount lottoCount = new LottoCount(val);
         }).isInstanceOf(InvalidLottoNumberValue.class);
     }
+
     @ParameterizedTest
-    @ValueSource(ints={1,100,0})
-    void 양수혹은0이면_정상실행(int val){
-        assertThatCode(()->{
+    @ValueSource(ints = {1, 100, 0})
+    void 양수혹은0이면_정상실행(int val) {
+        assertThatCode(() -> {
             LottoCount lottoCount = new LottoCount(val);
         }).doesNotThrowAnyException();
     }
