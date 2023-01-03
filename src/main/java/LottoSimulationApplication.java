3d5ce68e57e704f1ee76a9e179d4ecc49ec42fea
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-03 19:34:49 +0900

    refac: 에러 발생 시 재시도 하도록 변경

--- src/main/java/LottoSimulationApplication.java
+++ src/main/java/LottoSimulationApplication.java
@@ -3,6 +3,12 @@ import lotto.controller.LottoController;
 public class LottoSimulationApplication {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.startLottoGame();
+        while(true) {
+            try {
+                lottoController.startLottoGame();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
