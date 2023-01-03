6684b4ac8bd241d7e59a8e508bd7a71d5a7d8da5
yooj929 <jang0929@naver.com>
2023-01-03 16:35:49 +0900

    refactor: ArrayList -> Set으로 변경

--- src/main/java/lotto/factory/LottoNumbersFactory.java
+++ src/main/java/lotto/factory/LottoNumbersFactory.java
@@ -3,27 +3,30 @@ package lotto.factory;
 import static lotto.domain.LottoConstants.LOTTO_NUMBER_COUNT;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lotto.domain.LottoNumbers;
 import lotto.generatepolicy.GeneratePolicy;
 
 public class LottoNumbersFactory {
     public static LottoNumbers create(GeneratePolicy generatePolicy) {
-        List<Integer> numbers = new ArrayList<>();
-        while (isNotFull(numbers)) {
+        Set<Integer> numbers = new HashSet<>();
+        while(isNotFull(numbers)){
             addNumbers(numbers, generatePolicy.generate());
         }
-        numbers.sort(Integer::compare);
-        return new LottoNumbers(numbers);
+        List<Integer> result = new ArrayList<>(numbers);
+        Collections.sort(result);
+        return new LottoNumbers(result);
     }
 
-    private static boolean isNotFull(List<Integer> numbers) {
+    private static boolean isNotFull(Set<Integer> numbers) {
         return numbers.size() != LOTTO_NUMBER_COUNT;
     }
 
-    private static void addNumbers(List<Integer> numbers, int target) {
-        if (!numbers.contains(target)) {
-            numbers.add(target);
-        }
+    private static void addNumbers(Set<Integer> numbers, int target) {
+        numbers.add(target);
     }
 }
