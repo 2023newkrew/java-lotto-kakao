571cd9800823a36df97d7946c085e3969ddd837b
kjaehyeon <jacob9404@gmail.com>
2023-01-03 15:43:44 +0900

    test: LottoService 테스트 코드 작성

--- src/test/java/lottoTest/service/LottoServiceTest.java
+++ src/test/java/lottoTest/service/LottoServiceTest.java
@@ -1,4 +1,22 @@
 package lottoTest.service;
 
+import lotto.model.LottoResult;
+import lotto.service.LottoService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoServiceTest {
+    @Test
+    public void getRateOfReturnTest() {
+        //given
+        LottoResult lottoResult = new LottoResult(List.of(0, 0, 0, 0, 1, 0));
+        //when
+        LottoService lottoService = new LottoService();
+        Double rateOfReturn = lottoService.getRateOfReturn(20000, lottoResult.getRankCountMap());
+        //then
+        assertThat(rateOfReturn).isEqualTo(0.25);
+    }
 }
