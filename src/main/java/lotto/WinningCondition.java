f5e7a3e80cc00be6a253d83e7a7569a3d59a808e
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-03 15:12:56 +0900

    feat : WinningCondition enum 개인 코드 컨벤션 최적화

--- src/main/java/lotto/WinningCondition.java
+++ src/main/java/lotto/WinningCondition.java
@@ -1,37 +1,33 @@
 package lotto;
 
 public enum WinningCondition {
-    FIRST_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT,false),
+    FIRST_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT, false),
             new Cash(LottoConstants.FIRST_PRIZE_CASH), 4),
-    SECOND_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1,true),
+    SECOND_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1, true),
             new Cash(LottoConstants.SECOND_PRIZE_CASH), 3),
-    THIRD_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1,false),
+    THIRD_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1, false),
             new Cash(LottoConstants.THIRD_PRIZE_CASH), 2),
-    FOURTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-2,false),
+    FOURTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-2, false),
             new Cash(LottoConstants.FOURTH_PRIZE_CASH), 1),
-    FIFTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-3,false),
+    FIFTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-3, false),
             new Cash(LottoConstants.FIFTH_PRIZE_CASH), 0);
 
     private final LottoResult lottoResult;
     private final Cash winningPrize;
-    private int printOrder;
+    private final int printOrder;
 
-    WinningCondition(LottoResult lottoResult, Cash winningPrize, int printOrder){
+    WinningCondition(LottoResult lottoResult, Cash winningPrize, int printOrder) {
         this.lottoResult = lottoResult;
         this.winningPrize = winningPrize;
         this.printOrder = printOrder;
     }
 
-    public Cash getPrizeIfMatch(LottoResult lottoResult){
-        if (lottoResult.equals(this.lottoResult)){
+    public Cash getPrizeIfMatch(LottoResult lottoResult) {
+        if (lottoResult.equals(this.lottoResult)) {
             return winningPrize;
         }
-        return new Cash(0L);
-    }
 
-    @Override
-    public String toString() {
-        return lottoResult + "(" + winningPrize + ")";
+        return new Cash(0L);
     }
 
     public int getOrder() {
@@ -40,5 +36,10 @@ public enum WinningCondition {
 
     public LottoResult getLottoResult() {
         return lottoResult;
+    }
+
+    @Override
+    public String toString() {
+        return lottoResult + "(" + winningPrize + ")";
     }
 }
