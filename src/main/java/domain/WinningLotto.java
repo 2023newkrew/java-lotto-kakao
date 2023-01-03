cccd43724c24de41a2b04576ef1b3443af932862
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 22:01:03 +0900

    refac: 당첨 로또 번호의 상속 관계 제거

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WinningLotto extends Lotto {
+public class WinningLotto {
     private final List<Integer> winningLottoNumbers;
 
     public WinningLotto(String input) {
