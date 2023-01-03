edce4c4a6c86e4709d1ba8256f857a06b4efe71d
Minsu Kim <ravor8878@gmail.com>
2023-01-03 11:19:50 +0900

    feat : LottoResult(몇개 맞추고 보너스 맞는지 여부)추가

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import lotto.exception.InvalidLottoResult;
+
+import java.util.Objects;
+
+public class LottoResult {
+    private final int matchCount;
+    private final boolean matchBonus;
+    public LottoResult(int matchCount, boolean matchBonus){
+        if (matchCount < 0 || matchCount > 6 || (matchCount==6 && matchBonus)){
+            throw new InvalidLottoResult();
+        }
+        this.matchBonus = matchBonus;
+        this.matchCount = matchCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        LottoResult that = (LottoResult) o;
+        return matchCount == that.matchCount && matchBonus == that.matchBonus;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matchCount, matchBonus);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isMatchBonus() {
+        return matchBonus;
+    }
+
+/*    @Override
+    public String toString() {
+        String result = matchCount + "개 일치";
+        if (matchBonus){
+            result += ", 보너스 볼 일치";
+        }
+        return result;
+    }*/
+}
