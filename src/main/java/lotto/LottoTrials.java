173d153b68b7993fa23435c7ba570dcf950a41f8
Minsu Kim <ravor8878@gmail.com>
2023-01-03 10:40:38 +0900

    feat : LottoTrial들을 가지는 LottoTrials 구현

--- src/main/java/lotto/LottoTrials.java
+++ src/main/java/lotto/LottoTrials.java
@@ -1,5 +1,18 @@
 package lotto;
 
-public class LottoTrials {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoTrials {
+    private final List<LottoTrial> lottoTrials = new ArrayList<>();
+    public void add(LottoTrial lottoTrial){
+        lottoTrials.add(lottoTrial);
+    }
+
+    public int size(){
+        return lottoTrials.size();
+    }
+    public LottoTrial get(int index){
+        return lottoTrials.get(index);
+    }
 }
