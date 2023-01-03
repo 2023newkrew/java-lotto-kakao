3fd30ff2ff322ad212b0f4619ab6718c7fe3e7fa
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 16:45:49 +0900

    feat: 총 당첨 결과 일급 컬렉션 생성
    equals 메서드 오버라이딩
    put 메서드 구현

---
+++ src/main/java/domain/TotalResult.java
@@ -0,0 +1,39 @@
+package domain;
+
+import common.state.Result;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class TotalResult {
+    private final Map<Result, Integer> totalResult;
+
+    public TotalResult() {
+        totalResult = new HashMap<>();
+    }
+
+    public TotalResult(Map<Result, Integer> totalResult) {
+        this.totalResult = totalResult;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TotalResult that = (TotalResult) o;
+        return Arrays.stream(Result.values())
+                .allMatch(result -> this.totalResult.getOrDefault(result, 0) == that.totalResult.getOrDefault(result, 0));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalResult);
+    }
+
+    public void put(Result result) {
+        Integer previousValue = totalResult.getOrDefault(result, 0);
+        totalResult.put(result, previousValue + 1);
+    }
+}
