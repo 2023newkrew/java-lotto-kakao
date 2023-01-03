44002b65d0a0ec57e6b09181043ace163ab22b0b
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 13:16:38 +0900

    refac: winningLotto 인스턴스 변수 이름 변경
    winningLottoNumbers 로 이름 변경

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -3,19 +3,24 @@ package domain;
 import common.constant.Constants;
 import util.validator.WinningLottoValidator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinningLotto extends Lotto {
-    private final List<Integer> winningLotto;
+    private final List<Integer> winningLottoNumbers;
 
     public WinningLotto(String input) {
         WinningLottoValidator.validate(input);
         String[] splitInput = input.split(Constants.DELIMITER);
-        this.winningLotto = Arrays.stream(splitInput)
+        this.winningLottoNumbers = Arrays.stream(splitInput)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public List<Integer> getWinningLottoNumbers(){
+        return new ArrayList<>(winningLottoNumbers);
     }
 
 }
