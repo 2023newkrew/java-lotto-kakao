066bd07c9f7fccbadc92d82b5cc4fcf8bd94bc62
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-03 15:24:14 +0900

    feat : CashTest 테스트 개인 코드 컨벤션 최적화

--- src/test/java/lotto/CashTest.java
+++ src/test/java/lotto/CashTest.java
@@ -11,33 +11,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CashTest {
     @ParameterizedTest
-    @ValueSource(longs = {-1,-1000,-4000})
-    void 마이너스_금액인_경우_예외(long value){
-        assertThatThrownBy(()->{
+    @ValueSource(longs = {-1, -1000, -4000})
+    void 마이너스_금액인_경우_예외(long value) {
+        assertThatThrownBy(() -> {
             Cash cash = new Cash(value);
         }).isInstanceOf(InvalidCashValue.class);
     }
     @ParameterizedTest
-    @ValueSource(longs = {1,1000,40000000})
-    void 정상_금액인_경우_정상실행(long value){
-        assertThatCode(()->{
+    @ValueSource(longs = {1, 1000, 40000000})
+    void 정상_금액인_경우_정상실행(long value) {
+        assertThatCode(() -> {
             Cash cash = new Cash(value);
         }).doesNotThrowAnyException();
     }
     @Test
-    void 금액끼리_더할_수_있어야_한다(){
-        long val1=1000;
-        long val2=2000;
+    void 금액끼리_더할_수_있어야_한다() {
+        long val1 = 1000;
+        long val2 = 2000;
         Cash cash1 = new Cash(val1);
         Cash cash2 = new Cash(val2);
-        Cash cash3 = cash1.plus(val2);
+        Cash cash3 = cash1.plus(cash2);
 
         Assertions.assertThat(cash3.getCash()).isEqualTo(3000);
     }
 
     @Test
-    void toString의_경우_단위를_붙여야_한다(){
-        long val1=1000;
+    void toString의_경우_단위를_붙여야_한다() {
+        long val1 = 1000;
         Cash cash1 = new Cash(val1);
 
         Assertions.assertThat(cash1.toString()).isEqualTo("1000원");
