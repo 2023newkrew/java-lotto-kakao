b6cf900d3efe4be506a10d1ccc0504bd3203d121
kjaehyeon <jacob9404@gmail.com>
2023-01-03 14:11:46 +0900

    test: LottoWinningNumberTest에 LottoRank 테스트 추가

--- src/test/java/lottoTest/model/LottoWinningNumberTest.java
+++ src/test/java/lottoTest/model/LottoWinningNumberTest.java
@@ -2,6 +2,7 @@ package lottoTest.model;
 
 import lotto.exception.ErrorCode;
 import lotto.exception.LottoException;
+import lotto.model.LottoNumber;
 import lotto.model.LottoRank;
 import lotto.model.LottoTicket;
 import lotto.model.LottoWinningNumber;
@@ -11,20 +12,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LottoWinningNumberTest {
-    private final List<Integer> input = List.of(1,2,3,4,5,6);
+    private final List<LottoNumber> input = createLottoNumberList(List.of(1,2,3,4,5,6));
     @Test
     public void correctBonusBall() {
         //given
         LottoTicket lottoTicket = new LottoTicket(input);
 
         //when & then
-        assertDoesNotThrow(() -> new LottoWinningNumber(lottoTicket, 7));
+        assertDoesNotThrow(() -> new LottoWinningNumber(lottoTicket, new LottoNumber(7)));
     }
 
     @Test
@@ -33,7 +35,7 @@ public class LottoWinningNumberTest {
         LottoTicket lottoTicket = new LottoTicket(input);
 
         //when & then
-        assertThatThrownBy(() -> new LottoWinningNumber(lottoTicket, 1))
+        assertThatThrownBy(() -> new LottoWinningNumber(lottoTicket, new LottoNumber(1)))
                 .isInstanceOf(LottoException.class)
                 .hasMessage(ErrorCode.INVALID_BONUS_BALL.getMessage());
     }
@@ -49,12 +51,36 @@ public class LottoWinningNumberTest {
     }
     private static Stream<Arguments> checkRankTestGenerator(){
         return Stream.of(
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,3,4,5,6)), 7), LottoRank.RANK1),
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,3,4,5,8)), 6), LottoRank.RANK2),
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,3,4,5,8)), 7), LottoRank.RANK3),
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,3,4,8,9)), 7), LottoRank.RANK4),
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,3,8,9,10)), 7), LottoRank.RANK5),
-                Arguments.of(new LottoWinningNumber(new LottoTicket(List.of(1,2,8,9,10,11)), 7), LottoRank.RANK6)
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,3,4,5,6))), new LottoNumber(7)),
+                        LottoRank.RANK1
+                ),
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,3,4,5,8))), new LottoNumber(6)),
+                        LottoRank.RANK2)
+                ,
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,3,4,5,8))), new LottoNumber(7)),
+                        LottoRank.RANK3
+                ),
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,3,4,8,9))), new LottoNumber(7)),
+                        LottoRank.RANK4
+                ),
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,3,8,9,10))), new LottoNumber(7)),
+                        LottoRank.RANK5
+                ),
+                Arguments.of(
+                        new LottoWinningNumber(new LottoTicket(createLottoNumberList(List.of(1,2,8,9,10,11))), new LottoNumber(7)),
+                        LottoRank.RANK6
+                )
         );
+    }
+
+    private static List<LottoNumber> createLottoNumberList(List<Integer> integerList){
+        return integerList.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 }
