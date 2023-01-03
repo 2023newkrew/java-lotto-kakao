874359dd8fd63f4e4c907f52dd21b861d22396c0
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 15:44:14 +0900

    feat: 보너스 번호 도메인 생성

---
+++ src/main/java/domain/BonusNumber.java
@@ -0,0 +1,11 @@
+package domain;
+
+import util.validator.BonusNumberValidator;
+
+public class BonusNumber extends GeneralNumber {
+    public BonusNumber(WinningLotto winningLotto, String input) {
+        super(input);
+        BonusNumberValidator.validate(winningLotto, this.number);
+    }
+
+}
--- src/main/java/domain/GeneralNumber.java
+++ src/main/java/domain/GeneralNumber.java
@@ -3,7 +3,7 @@ package domain;
 import util.validator.SingleNumberValidator;
 
 public class GeneralNumber {
-    private final int number;
+    protected final int number;
     public GeneralNumber(String input) {
         SingleNumberValidator.validate(input);
         number = Integer.parseInt(input);
