e3d929e35e30746a3944031cd0adfd0a3be87dcc
park0503 <hooo0503@gmail.com>
2023-01-03 21:54:13 +0900

    refactor: 복수의 로또 생성 로직 LottoGenerator로 이관

--- src/main/java/lotto/models/LottoGenerator.java
+++ src/main/java/lotto/models/LottoGenerator.java
@@ -1,7 +1,10 @@
 package lotto.models;
 
-import static lotto.common.LottoConfiguration.*;
+import static lotto.common.LottoConfiguration.LOTTO_COUNT;
+import static lotto.common.LottoConfiguration.MAX_VALUE;
+import static lotto.common.LottoConfiguration.MIN_VALUE;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,11 +22,17 @@ public class LottoGenerator {
 
     public Lotto createLotto() {
         Collections.shuffle(lottoPreset);
-
         List<Integer> numbers = lottoPreset.subList(0, LOTTO_COUNT).stream()
                 .sorted()
                 .collect(Collectors.toList());
-
         return new Lotto(numbers);
+    }
+
+    public List<Lotto> createLottos(int numberOfLotto) {
+        List<Lotto> list = new ArrayList<>();
+        for (int i = 0; i < numberOfLotto; i++) {
+            list.add(createLotto());
+        }
+        return list;
     }
 }
--- src/test/java/lotto/models/LottoGeneratorTest.java
+++ src/test/java/lotto/models/LottoGeneratorTest.java
@@ -1,21 +1,35 @@
 package lotto.models;
 
+import static lotto.common.LottoConfiguration.LOTTO_COUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoGeneratorTest {
+
+    private final LottoGenerator lottoGenerator = new LottoGenerator();
+
     @Test
     @DisplayName("자동으로 로또를 생성할 수 있다.")
     public void testCreateLotto() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
         List<Integer> lotto = lottoGenerator.createLotto().getNumbers();
         assertThat(lotto).hasSize(6);
         assertThat(lotto).isSorted();
         for (Integer number : lotto) {
             assertThat(number).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(45);
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100, 1000})
+    @DisplayName("자동으로 다수의 로또를 생성할 수 있다.")
+    public void testCreateLottos(Integer input) {
+        List<Lotto> lottos = lottoGenerator.createLottos(input);
+        assertThat(lottos).hasSize(input);
+        lottos.forEach((lotto -> assertThat(lotto.getNumbers()).hasSize(LOTTO_COUNT)));
     }
 }
