09c2f793522470ab2dd53cf842aa85f35e7ed771
Eithea <oguraff94@gmail.com>
2023-01-03 23:29:44 +0900

    refactor : retype Prize and rename PrizeRecord

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,12 +1,12 @@
 package lotto.model;
 
 public enum Prize {
-    FIRST("6개 일치",2000000000),
-    SECOND("5개 일치, 보너스 볼 일치",30000000),
-    THIRD("5개 일치", 1500000),
-    FOURTH("4개 일치", 50000),
-    FIFTH("3개 일치", 5000),
-    LOSING("", 0);
+    FIRST("6개 일치",2000000000L),
+    SECOND("5개 일치, 보너스 볼 일치",30000000L),
+    THIRD("5개 일치", 1500000L),
+    FOURTH("4개 일치", 50000L),
+    FIFTH("3개 일치", 5000L),
+    LOSING("", 0L);
 
     private final String matchDescription;
     private final long prize;
