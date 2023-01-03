1bbb3e3dbaa3acfbf398ba0f87a7b4916d593f88
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 16:44:34 +0900

    test: 당첨 통계 확인 테스트 추가

--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -1,6 +1,10 @@
 package domain;
 
+import common.state.Result;
 import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -9,11 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottosTest {
 
     private static Lottos lottos;
+    private static WinningLotto winningLotto;
+    private static BonusNumber bonusNumber;
 
     @BeforeAll
     static void setUp() {
         lottos = new Lottos();
-        lottos.add("3, 5, 11, 16, 32, 38");
+        winningLotto = new WinningLotto("1, 2, 3, 4, 5, 6");
+        bonusNumber = new BonusNumber(winningLotto, "7");
+
     }
 
     @Test
@@ -23,6 +31,28 @@ public class LottosTest {
         lottos.add(newLottoNumbers);
         int afterSize = lottos.getSize();
         assertThat(afterSize).isEqualTo(beforeSize + 1);
+    }
+
+
+
+    @DisplayName("당첨 통계 확인 테스트")
+    @Test
+    void getTotalResultTest() {
+        String matchZero = "7, 8, 9, 10, 11, 12";
+        String matchOne = "1, 7, 8, 9, 10, 11";
+        String matchTwo = "1, 2, 7, 8, 9, 10";
+        String matchThree = "1, 2, 3, 7, 8, 9";
+        String matchFiveBonus = "1, 2, 3, 4, 5, 7";
+        String matchSix = "1, 2, 3, 4, 5, 6";
+        lottos.add(matchZero);
+        lottos.add(matchOne);
+        lottos.add(matchTwo);
+        lottos.add(matchThree);
+        lottos.add(matchFiveBonus);
+        lottos.add(matchSix);
+        Map<Result, Integer> expected = Map.of(Result.NONE, 3, Result.THREE, 1, Result.FIVEBONUS, 1, Result.SIX, 1);
+        assertThat(lottos.getTotalResult(winningLotto, bonusNumber)).isEqualTo(new TotalResult(expected));
+
     }
 
 }
