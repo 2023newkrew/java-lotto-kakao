0aabf3601e722fc143d56fec8790573e1a47ed29
HQkim <hyunkyu001@gmail.com>
2023-01-03 09:41:09 +0900

    refactor: UserLottoTest와 UserLotto 삭제

--- src/main/java/lotto/domain/UserLotto.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain;
-
-public class UserLotto {
-    private final LottoNumbers lottoNumbers;
-
-    public UserLotto(LottoNumbers lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
-    }
-}
