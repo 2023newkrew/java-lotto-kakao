0c47a64d1310313a35a3535f2addf7b1bd0c17fa
abcworld123 <rkaxhdals@gmail.com>
2023-01-03 13:44:04 +0900

    test: `LottoNumber` 일급컬렉션으로 분리
    테스트 코드 수정
    - 일급컬렉션 분리에 따른 테스트 코드 수정

--- src/test/java/lotto/LottoNumberGenerateTest.java
+++ src/test/java/lotto/LottoNumberGenerateTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -36,11 +38,11 @@ class LottoNumberGenerateTest {
     @DisplayName("로또 번호가 6개가 아니면 에러를 던진다.")
     void lottoInvalidNumbersTest1() {
         assertThatThrownBy(()->{
-            new LottoNumber(List.of(1,2,3,4));
+            new Lotto(Stream.of(1,2,3,4).map(LottoNumber::new).collect(Collectors.toList()));
         }).isInstanceOf(RuntimeException.class);
 
         assertThatThrownBy(()->{
-            new LottoNumber(List.of(1,2,3,4,5,6,7));
+            new Lotto(Stream.of(1,2,3,4,5,6,7).map(LottoNumber::new).collect(Collectors.toList()));
         }).isInstanceOf(RuntimeException.class);
     }
 
@@ -48,11 +50,11 @@ class LottoNumberGenerateTest {
     @DisplayName("로또 번호가 1 미만 45초과의 숫자를 가지면 에러를 던진다.")
     void lottoInvalidNumbersTest2() {
         assertThatThrownBy(()->{
-            new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 100));
+            new Lotto(Stream.of(1,2,3,4,5,100).map(LottoNumber::new).collect(Collectors.toList()));
         }).isInstanceOf(RuntimeException.class);
 
         assertThatThrownBy(()->{
-            new LottoNumber(Arrays.asList(0, 2, 3, 4, 5, 6));
+            new Lotto(Stream.of(0,2,3,4,5,6).map(LottoNumber::new).collect(Collectors.toList()));
         }).isInstanceOf(RuntimeException.class);
     }
 
@@ -60,7 +62,7 @@ class LottoNumberGenerateTest {
     @DisplayName("로또 번호에 중복이 있으면 에러를 던진다.")
     void lottoInvalidNumberTest3() {
         assertThatThrownBy(()->{
-            new LottoNumber(Arrays.asList(1, 1, 3, 4, 5, 6));
+            new Lotto(Stream.of(1,1,3,4,5,6).map(LottoNumber::new).collect(Collectors.toList()));
         }).isInstanceOf(RuntimeException.class);
     }
 }
