642a711384c71af2fdb9214c970cf52e2be894b2
Subin Min <tnqls5417@naver.com>
2023-01-03 15:16:20 +0900

    refactor: NumberParser 패키지 이동 및 static 방식으로 변경

--- src/main/java/utils/NumberParser.java
+++ src/main/java/utils/NumberParser.java
@@ -1,16 +1,18 @@
+package utils;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class NumberParser {
-    public List<Integer> splitAndParse(String input) {
+    public static List<Integer> splitAndParse(String input) {
         List<String> result = List.of(input.split(", "));
         return result
                 .stream()
-                .map(this::parse)
+                .map(NumberParser::parse)
                 .collect(Collectors.toList());
     }
 
-    public Integer parse(String input) {
+    public static Integer parse(String input) {
         int number = 0;
         try {
             number = Integer.parseInt(input);
