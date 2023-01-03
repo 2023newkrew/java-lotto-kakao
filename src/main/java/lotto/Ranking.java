63784f9ade127232e67b220de29d9f9d733a2a9b
kane.woz <kane.woz@kakaocorp.com>
2023-01-03 13:45:31 +0900

    docs: 이해를 위한 코드에 javadoc 추가

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -2,6 +2,9 @@ package lotto;
 
 import java.util.Map;
 
+/**
+ * 로또의 등수와 각 등수의 상금에 대한 정의가 포함되어 있습니다.
+ */
 public enum Ranking {
     FIRST(new Price(2000000000)),
     SECOND(new Price(30000000)),
--- src/main/java/lotto/Ratio.java
+++ src/main/java/lotto/Ratio.java
@@ -2,11 +2,19 @@ package lotto;
 
 import java.util.Objects;
 
+/**
+ * @param <T> 비율로 사용할 타입 지정 {@link java.lang.Number} 타입이라면 모두 비율 측정의 용도로 사용 가능합니다.
+ */
 public class Ratio<T extends Number> extends Number {
-    // 해당 오차 이내의 값은 동일한 값으로 취급합니다.
-    // IEEE754 한계로 정확한 비교는 불가능하고 Bigint를 사용하지 않기에 불가피하게 이 개념을 도입합니다.
-    // 기본 Machine Epsilon보다 둔감하게 설정했습니다. 이는 의도된 사항으로 약간의 오차를 허용하기 위함입니다.
-    // IEEE754 binary64 Machine Epsilon = 2^-52
+    /**
+     * 해당 오차 이내의 값은 동일한 값으로 취급합니다.
+     * <p>
+     * IEEE754 한계로 정확한 비교는 불가능하고 Bigint를 사용하지 않기에 불가피하게 이 개념을 도입합니다.
+     * <p>
+     * 기본 Machine Epsilon보다 둔감하게 설정했습니다. 이는 의도된 사항으로 약간의 오차를 허용하기 위함입니다.
+     * <p>
+     * 참조 : IEEE754 binary64 Machine Epsilon = 2^-52
+     */
     private static final double SUPPRESS_EPSILON = 0.0000001d; // := 1^-7
 
     private final T numerator; // 분자
