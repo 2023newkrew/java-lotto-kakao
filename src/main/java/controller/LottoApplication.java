5c05f082f5f473e28f4d1555d7912eaacd2ebd89
HONGMINJU <mhong0800@gmail.com>
2023-01-03 16:49:39 +0900

    style: 에러메세지만 출력

--- src/main/java/controller/LottoApplication.java
+++ src/main/java/controller/LottoApplication.java
@@ -32,7 +32,7 @@ public class LottoApplication {
             outputView.printLottoStatistics(lottoResult);
         }
         catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 }
