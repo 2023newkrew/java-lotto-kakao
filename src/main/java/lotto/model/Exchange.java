55e2435564adf630813630ba1b6543b0ab7ee0bf
Eithea <oguraff94@gmail.com>
2023-01-04 01:55:06 +0900

    feat : add yield calculation method

--- src/main/java/lotto/model/Exchange.java
+++ src/main/java/lotto/model/Exchange.java
@@ -7,17 +7,29 @@ public class Exchange {
         return cash >= Exchange.LOTTO_PRICE;
     }
 
-    public Lotto cashToLotto(long cash) {
+    public Lotto purchaseLotto(long cash) {
         Lotto lotto = new Lotto();
         lotto.purchaseRandomLotto(cash / Exchange.LOTTO_PRICE);
         return lotto;
     }
 
-    public long prizeToCash(PrizeRecord prizeRecord) {
+    public long getPrizeAmount(PrizeRecord prizeRecord) {
         long cash = 0;
         for (Prize prize : Prize.values()) {
             cash += prizeRecord.getCountOf(prize) * prize.prize();
         }
         return cash;
+    }
+
+    private long getPurchaseAmount(PrizeRecord prizeRecord) {
+        int quantity = 0;
+        for (Prize prize : Prize.values()) {
+            quantity += prizeRecord.getCountOf(prize);
+        }
+        return Exchange.LOTTO_PRICE * quantity;
+    }
+
+    public double calculateYield(PrizeRecord prizeRecord) {
+        return (double) this.getPrizeAmount(prizeRecord) / this.getPurchaseAmount(prizeRecord);
     }
 }
