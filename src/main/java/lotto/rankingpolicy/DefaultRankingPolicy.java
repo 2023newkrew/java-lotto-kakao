152fb213a60951e22bbf79d4c0c368f50c9856d7
yooj929 <jang0929@naver.com>
2023-01-03 16:22:42 +0900

    fix: DefaultRankingPolicy 중복 코드 제거

--- src/main/java/lotto/rankingpolicy/DefaultRankingPolicy.java
+++ src/main/java/lotto/rankingpolicy/DefaultRankingPolicy.java
@@ -11,14 +11,11 @@ import lotto.domain.LottoResult;
 
 public class DefaultRankingPolicy implements RankingPolicy {
 
-    private Map<DefaultLottoResult, LottoRank> lottoResultLottoRankMap = new HashMap<>();
+    private final Map<DefaultLottoResult, LottoRank> lottoResultLottoRankMap = new HashMap<>();
 
     public DefaultRankingPolicy() {
         lottoResultLottoRankMap.put(new DefaultLottoResult(FIRST.getMatchCount(), FIRST.isRequiredBonus()), FIRST);
         lottoResultLottoRankMap.put(new DefaultLottoResult(SECOND.getMatchCount(), SECOND.isRequiredBonus()), SECOND);
-        lottoResultLottoRankMap.put(new DefaultLottoResult(THIRD.getMatchCount(), THIRD.isRequiredBonus()), THIRD);
-        lottoResultLottoRankMap.put(new DefaultLottoResult(FOURTH.getMatchCount(), FOURTH.isRequiredBonus()), FOURTH);
-        lottoResultLottoRankMap.put(new DefaultLottoResult(FIFTH.getMatchCount(), FIFTH.isRequiredBonus()), FIFTH);
         lottoResultLottoRankMap.put(new DefaultLottoResult(THIRD.getMatchCount(), THIRD.isRequiredBonus()), THIRD);
         lottoResultLottoRankMap.put(new DefaultLottoResult(FOURTH.getMatchCount(), FOURTH.isRequiredBonus()), FOURTH);
         lottoResultLottoRankMap.put(new DefaultLottoResult(FIFTH.getMatchCount(), FIFTH.isRequiredBonus()), FIFTH);
