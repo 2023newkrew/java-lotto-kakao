f47e8241b2da332d577bd79464daff6e39b36810
jungin0507 <a01033046758@gmail.com>
2023-01-03 14:39:31 +0900

    refactor: 당첨 통계 출력 로직 변경

--- src/main/java/view/LottoOutputView.java
+++ src/main/java/view/LottoOutputView.java
@@ -5,11 +5,13 @@ import domain.lotto.LottoResult;
 import domain.lotto.prize.LottoPrize;
 import domain.lotto.ticket.LottoTicket;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LottoOutputView {
+
+    private static final String resultMessageFormat = "%s (%d원)-%d개";
+
     public void printLottoTickets(List<LottoTicket> lottoTickets) {
         printLottoTicketPurchaseCount(lottoTickets);
         lottoTickets.stream()
@@ -25,17 +27,29 @@ public class LottoOutputView {
     }
 
     private void printPrizeCount(Map<LottoPrize, Integer> prizeCounts) {
-        System.out.println("3개 일치 (" + LottoPrize.FIFTH_PRIZE.getPrizeMoney() + "원)-"
-                + prizeCounts.get(LottoPrize.FIFTH_PRIZE) + "개");
-        System.out.println("4개 일치 (" + LottoPrize.FOURTH_PRIZE.getPrizeMoney() + "원)-"
-                + prizeCounts.get(LottoPrize.FOURTH_PRIZE) + "개");
-        System.out.println("5개 일치 (" + LottoPrize.THIRD_PRIZE.getPrizeMoney() + "원)-"
-                + prizeCounts.get(LottoPrize.THIRD_PRIZE) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (" + LottoPrize.SECOND_PRIZE.getPrizeMoney() + "원)-"
-                + prizeCounts.get(LottoPrize.SECOND_PRIZE) + "개");
-        System.out.println("6개 일치 (" + LottoPrize.FIRST_PRIZE.getPrizeMoney() + "원)-"
-                + prizeCounts.get(LottoPrize.FIRST_PRIZE) + "개");
+        Map<LottoPrize, String> prizeDescriptionMap = makePrizeDescription();
+        List<LottoPrize> lottoPrizes = Arrays.asList(LottoPrize.values());
+        Collections.reverse(lottoPrizes);
+
+        for (LottoPrize lottoPrize : lottoPrizes) {
+            String resultMessage = String.format(resultMessageFormat,
+                    prizeDescriptionMap.get(lottoPrize),
+                    lottoPrize.getPrizeMoney(),
+                    prizeCounts.get(lottoPrize));
+            System.out.println(resultMessage);
+        }
     }
+
+    private Map<LottoPrize, String> makePrizeDescription() {
+        Map<LottoPrize, String> prizeDescriptionMap = new HashMap<>();
+        prizeDescriptionMap.put(LottoPrize.FIFTH_PRIZE, "3개 일치");
+        prizeDescriptionMap.put(LottoPrize.FOURTH_PRIZE, "4개 일치");
+        prizeDescriptionMap.put(LottoPrize.THIRD_PRIZE, "5개 일치");
+        prizeDescriptionMap.put(LottoPrize.SECOND_PRIZE, "5개 일치, 보너스 볼 일치");
+        prizeDescriptionMap.put(LottoPrize.FIRST_PRIZE, "6개 일치");
+        return prizeDescriptionMap;
+    }
+
 
     private void printEarningRate(double earningRate) {
         System.out.printf("총 수익률은 %.2f입니다.\n", earningRate);
