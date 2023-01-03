efc3b34de3e407a776515945c27b1d4679f472c9
baker.gus <baker.gus@kakaocorp.com>
2023-01-03 13:22:37 +0900

    feat: LotteryNumbers에서 중복 숫자 검사

--- src/main/java/lotto/LotteryNumbers.java
+++ src/main/java/lotto/LotteryNumbers.java
@@ -5,12 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class LotteryNumbers {
+    private static final int LOTTERY_NUMBER_SIZE = 6;
     private final List<LotteryNumber> numbers = new ArrayList<>();
 
     public LotteryNumbers(List<Integer> numbers) {
+        validateNumbers(numbers);
+
         for (Integer number : numbers) {
             this.numbers.add(new LotteryNumber(number));
         }
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        if (!isValidSize(numbers) || isDuplicateNumber(numbers))
+            throw new IllegalArgumentException("잘못된 로또 번호 입력입니다");
+    }
+
+    private boolean isValidSize(List<Integer> numbers) {
+        return numbers.size() == LOTTERY_NUMBER_SIZE;
+    }
+
+    private boolean isDuplicateNumber(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
     }
 
     public List<LotteryNumber> getNumbers() {
