93af6f51bfe3a4090b9dd2b4d9cc4dacb4e48839
yooj929 <jang0929@naver.com>
2023-01-03 14:59:00 +0900

    refactor: inputView 정리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,35 +13,32 @@ public class InputView {
 
     public int readPrice() {
         String line = scanner.nextLine();
-        validateNPE(line);
         int price = parseInt(line);
         validatePrice(price);
         return price;
-
     }
 
     public List<Integer> readLottoAnswerNumbers() {
-        return Arrays.stream(scanner.nextLine().split(", ")).map(Integer::parseInt).collect(Collectors.toList());
+        return Arrays.stream(scanner.nextLine().split(", ")).map(this::parseInt).collect(Collectors.toList());
     }
 
     public Integer readBonusBall() {
         String line = scanner.nextLine();
-        validateNPE(line);
         return parseInt(line);
     }
 
     private void validateNPE(String line) {
         if (StringUtils.isNullOrBlank(line)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("널 또는 빈 문자열이 올 수 없습니다.");
         }
     }
 
     public int parseInt(String num) {
+        validateNPE(num);
         try {
             return Integer.parseInt(num);
-
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자가 주어져야 합니다.");
         }
 
     }
@@ -53,14 +50,14 @@ public class InputView {
 
     private void validateRange(int price) {
         if (price < LOTTO_PRICE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1000원 이상이 주어져야합니다.");
         }
 
     }
 
     private void validateIsValid(int price) {
         if (price % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1000의 배수가 주어져야합니다.");
         }
     }
 }
