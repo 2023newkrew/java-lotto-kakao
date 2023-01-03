3d6fdd3f8cc2659a13d61ec3a22b150e2fb23c5f
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 10:22:27 +0900

    test: 당첨 번호의 숫자 중복 확인 테스트 코드 작성

--- src/test/java/util/validator/WinningNumbersValidatorTest.java
+++ src/test/java/util/validator/WinningNumbersValidatorTest.java
@@ -1,6 +1,5 @@
 package util.validator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,15 +11,22 @@ public class WinningNumbersValidatorTest {
     @DisplayName("입력된 당첨 숫자의 개수가 알맞지 않은지 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
-    void validateThrowIllegalArgumentExceptionTest(String inputSizeNotSix) {
-        assertThrows(IllegalArgumentException.class, () -> WinningNumbersValidator.validate(inputSizeNotSix));
+    void validateLengthThrowIllegalArgumentExceptionTest(String inputLengthNotSix) {
+        assertThrows(NumberFormatException.class, () -> WinningNumbersValidator.validate(inputLengthNotSix));
     }
 
     @DisplayName("입력된 당첨 숫자의 개수가 알맞은지 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5, 6", "7, 8, 9, 10, 11, 12"})
-    void validateDoesNotThrowIllegalArgumentExceptionTest(String inputSizeIsSix) {
-        assertDoesNotThrow(() -> WinningNumbersValidator.validate(inputSizeIsSix));
+    void validateLengthDoesNotThrowIllegalArgumentExceptionTest(String inputLengthIsSix) {
+        assertDoesNotThrow(() -> WinningNumbersValidator.validate(inputLengthIsSix));
+    }
+
+    @DisplayName("입력된 숫자가 모두 다른지 확인하는 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"2, 2, 3, 4, 5, 6", "1, 1, 1, 1, 1, 1"})
+    void validateDuplicateThrowIllegalArgumentExceptionTest(String inputDuplicate) {
+        assertThrows(IllegalArgumentException.class, () -> WinningNumbersValidator.validate(inputDuplicate));
     }
 
 }
