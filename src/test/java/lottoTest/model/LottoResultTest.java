1185fda5a6102d64085245e3aa66f6ccf1da7112
kjaehyeon <jacob9404@gmail.com>
2023-01-03 15:34:02 +0900

    test: LottoResult 테스트 작성

--- src/test/java/lottoTest/model/LottoResultTest.java
+++ src/test/java/lottoTest/model/LottoResultTest.java
@@ -1,4 +1,25 @@
 package lottoTest.model;
 
+import lotto.model.LottoRank;
+import lotto.model.LottoResult;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoResultTest {
+
+    @ParameterizedTest
+    @EnumSource(LottoRank.class)
+    void addLottoRankTest(LottoRank lottoRank){
+        //given
+        LottoResult lottoResult = new LottoResult();
+
+        //when
+        lottoResult.addLottoRank(lottoRank);
+
+        //then
+        assertThat(lottoResult.getRankCountMap().get(lottoRank)).isEqualTo(1);
+    }
+
 }
