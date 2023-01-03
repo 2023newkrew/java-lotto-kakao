077783ad632655817ac94538199c7a6b507410f2
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-03 15:22:13 +0900

    feat : Main 클래스 개인 코드 컨벤션 최적화

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static Cash cash;
-    static LottoCount lottoCount;
-    static LottoTrials lottoTrials = new LottoTrials();
-    static WinNumber winNumber;
-    static TotalResult totalResult = new TotalResult();
+    public static Scanner sc = new Scanner(System.in);
+    public static Cash cash;
+    public static LottoCount lottoCount;
+    public static LottoTrials lottoTrials = new LottoTrials();
+    public static WinNumber winNumber;
+    public static TotalResult totalResult = new TotalResult();
+
     public static void main(String[] args) {
         try {
-            input();
+            inputCash();
             createLotto(lottoCount);
             inputWinningLotto();
             processLotto();
@@ -23,34 +24,46 @@ public class Main {
             System.out.println(e.getClass() + " : " + e.getMessage());
         }
     }
-    private static void input(){
+
+    private static void inputCash() {
         System.out.println("구입금액을 입력해 주세요.");
+
         cash = new Cash(Long.parseLong(sc.nextLine()));
+
         lottoCount = new LottoCount(cash);
-        System.out.println(lottoCount+"를 구매했습니다.");
+
+        System.out.println(lottoCount + "를 구매했습니다.");
     }
-    private static void createLotto(LottoCount lottoCount){
-        for (int i=1;i<=lottoCount.getCount();i++){
+
+    private static void createLotto(LottoCount lottoCount) {
+        for (int i = 0; i < lottoCount.getCount(); i++){
             LottoTrial lottoTrial = new LottoTrialRandom(new LottoPickerRandom());
+
             System.out.println(lottoTrial);
+
             lottoTrials.add(lottoTrial);
         }
     }
 
     private static void inputWinningLotto() {
         System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+
         List<LottoBallNumber> winNum = Arrays.stream(sc.nextLine().split(","))
                 .map(v -> new LottoBallNumber(Integer.parseInt(v.trim())))
                 .collect(Collectors.toList());
+
         System.out.println("보너스 볼을 입력해 주세요.");
+
         winNumber = new WinNumber(new LottoTrialManual(winNum), new LottoBallNumber(sc.nextInt()));
+
         System.out.println();
     }
 
-    private static void processLotto(){
-        for (int i=0; i<lottoTrials.size();i++){
+    private static void processLotto() {
+        for (int i = 0; i < lottoCount.getCount(); i++){
             totalResult.addResult(winNumber.compareLotto(lottoTrials.get(i)));
         }
+
         System.out.println(totalResult);
     }
 }
