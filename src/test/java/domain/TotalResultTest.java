bf8ca1f2ad23acf76da8b3ad5e1dab1c7f26ee9e
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 17:35:09 +0900

    test: 수익률 확인 테스트 케이스 추가

--- src/test/java/domain/TotalResultTest.java
+++ src/test/java/domain/TotalResultTest.java
@@ -23,8 +23,13 @@ class TotalResultTest {
 
     @Test
     void getProfitTest() {
+        int paidPrice = 14_000;
         Map<Result, Integer> seedTotalResult = Map.of(Result.THREE, 1);
         TotalResult totalResult = new TotalResult(seedTotalResult);
-        assertThat(totalResult.getProfit()).isEqualTo(0.35);
+        assertThat(totalResult.getProfit(paidPrice)).isEqualTo(0.35);
+
+        Map<Result, Integer> seedTotalResult2 = Map.of(Result.THREE, 2, Result.SIX,1);
+        TotalResult totalResult2 = new TotalResult(seedTotalResult2);
+        assertThat(totalResult2.getProfit(paidPrice)).isEqualTo(142857.85);
     }
 }
