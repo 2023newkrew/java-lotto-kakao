3227a8fb2b4926fb41a379d42038d33b80889a25
abcworld123 <rkaxhdals@gmail.com>
2023-01-03 16:15:38 +0900

    feat: 랜덤한 숫자와 로또를 발행하는 generator 구현
    - start - end까지의 랜덤한 숫자 n개를 중복 없이 발행하는 RandomNumberGenerator 구현
    - RandomNumber 기반 RandomLottoGenerator 구현

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class RandomNumberGenerator {
+    private static Random random = new Random();
+
+    public static List<Integer> getRandomNumbers(int n, int start, int end) {
+        if(end - start + 1 < n) throw new RuntimeException();
+        return random.ints(start, end).distinct().limit(n).boxed().collect(Collectors.toList());
+    }
+}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,15 +2,19 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.Lottos;
+import lotto.util.RandomLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class LottoTest {
+class LottoTest {
     @Test
     @DisplayName("로또 당첨번호가 몇개 인지 반환")
     void iAmRich() {
@@ -25,5 +29,11 @@ public class LottoTest {
         Lotto myNumber = new Lotto(Stream.of(1,2,3,4,5,6).map(LottoNumber::new).collect(Collectors.toList()));
         LottoNumber bonus = new LottoNumber(5);
         assertThat(myNumber.hasBonus(bonus)).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 열장을 발행한다.")
+    void tooManyLottos() {
+        assertThat(new RandomLottoGenerator().generateLottos(10).size()).isEqualTo(10);
     }
 }
