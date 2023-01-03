c4df1c29d05485fdd1971d6a4245a42661870fac
HQkim <hyunkyu001@gmail.com>
2023-01-03 09:23:26 +0900

    refactor: 매직넘버 상수로 변환

--- src/main/java/lotto/domain/SingleLottoNumber.java
+++ src/main/java/lotto/domain/SingleLottoNumber.java
@@ -4,9 +4,11 @@ import java.util.Objects;
 
 public class SingleLottoNumber {
     private final int singleLottoNumber;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     public SingleLottoNumber(int singleLottoNumber) {
-        if (singleLottoNumber < 1 || singleLottoNumber > 45) {
+        if (singleLottoNumber < MIN_LOTTO_NUMBER || singleLottoNumber > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("로또의 각 숫자는 1이상 45 이하여야 합니다.");
         }
 
