80c87bb23eda79a8f37de55379a7fd33cccaad28
Minsu Kim <ravor8878@gmail.com>
2023-01-03 11:20:14 +0900

    feat : WinNumber에 compareLotto 기능 추가

---
+++ src/test/java/lotto/WinNumberTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import lotto.exception.DuplicatedBallNumber;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.DefaultTextUI;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinNumberTest {
+    @Test
+    void 보너스_숫자가_원래_숫자와_겹치면_예외(){
+        assertThatThrownBy(()-> {
+            WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), new LottoBallNumber(6));
+        }).isInstanceOf(DuplicatedBallNumber.class);
+    }
+    @Test
+    void 보너스_숫자가_원래_숫자와_안겹치면_정상실행(){
+        assertThatCode(()-> {
+            WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), new LottoBallNumber(7));
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    void trial에_대한_결과를_잘_반환하는가(){
+        WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), new LottoBallNumber(7));
+
+        Assertions.assertThat(winNumber.compareLotto(new LottoTrialManual(List.of(1,2,3,4,5,7))))
+                .isEqualTo(new LottoResult(5,true));
+
+        Assertions.assertThat(winNumber.compareLotto(new LottoTrialManual(List.of(4,2,3,1,8,9))))
+                .isEqualTo(new LottoResult(4,false));
+    }
+}
