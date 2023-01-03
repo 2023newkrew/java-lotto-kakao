d9bb6384926b793fdd6e474541edf7fcfeacc5c5
yooj929 <jang0929@naver.com>
2023-01-03 15:20:41 +0900

    refactor: Appconfig 정리

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -11,11 +11,10 @@ public class AppConfig {
     private static final InputView inputView = new InputView();
     private static final RankingPolicy rankingPolicy = new DefaultRankingPolicy();
     private static final OutputView outputView = new OutputView();
-    private final LottoGame lottoGame;
     private final LottoController lottoController;
 
     public AppConfig() {
-        this.lottoGame = new LottoGame(rankingPolicy);
+        LottoGame lottoGame = new LottoGame(rankingPolicy);
         this.lottoController = new LottoController(inputView, outputView, lottoGame);
     }
 
