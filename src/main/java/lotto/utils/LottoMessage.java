6dcf56e8eb00f1a0b164713d142f001e03d9e8a1
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-03 13:44:45 +0900

    feat(lotto): inputview 구현

--- src/main/java/lotto/utils/LottoMessage.java
+++ src/main/java/lotto/utils/LottoMessage.java
@@ -1,7 +1,9 @@
 package lotto.utils;
 
 public enum LottoMessage {
-    INPUT_AMOUNT("구입금액을 입력해 주세요.");
+    INPUT_AMOUNT("구입금액을 입력해 주세요."),
+    INPUT_WIN_NUMBERS("지난 주 당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_BALL("보너스 볼을 입력해 주세요.");
 
     private final String message;
 
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+import static lotto.utils.LottoMessage.*;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int inputUserAmount(){
+        System.out.println(INPUT_AMOUNT.getMessage());
+        return scanner.nextInt();
+    }
+
+    public String inputWinNumber(){
+        System.out.println(INPUT_WIN_NUMBERS.getMessage());
+        return scanner.nextLine();
+    }
+
+    public int inputBonusNumber(){
+        System.out.println(INPUT_BONUS_BALL.getMessage());
+        return scanner.nextInt();
+    }
+}
