58fc2e2b35a138a14feb052d521cc0458c1f9177
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-03 09:45:05 +0900

    fix(lotto): 랜덤 로직을 shuffle을 사용하도록 변경

--- src/main/java/lotto/domain/LottoRandom.java
+++ src/main/java/lotto/domain/LottoRandom.java
@@ -1,23 +1,25 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoRandom {
     private final Random random;
     private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_MAX_NUMBER = 45;
 
     public LottoRandom(){
         this.random = new Random();
     }
 
     public ArrayList<Integer> createRandomNumbers(){
-        Set<Integer> randomNumbersSet = new HashSet<>();
-        while (randomNumbersSet.size() != LOTTO_NUMBER_COUNT) {
-            randomNumbersSet.add(random.nextInt(45)+1);
-        }
-        return new ArrayList<>(randomNumbersSet);
+        List<Integer> numList = IntStream.range(1, LOTTO_MAX_NUMBER + 1)
+                .boxed()
+                .collect(Collectors.toList());
+        Collections.shuffle(numList);
+        numList = numList.subList(0, LOTTO_NUMBER_COUNT);
+        Collections.sort(numList);
+        return new ArrayList<>(numList);
     }
 }
