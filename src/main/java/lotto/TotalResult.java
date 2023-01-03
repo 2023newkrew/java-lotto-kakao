e1ac253cb8ea1c073a2e643237fda3b050385572
Minsu Kim <ravor8878@gmail.com>
2023-01-03 14:54:18 +0900

    feat : getLottoCountOfResult 추가 및 내부 객체 수정
    toString에서 표시되는 정보는 public 메서드로 구할 수 있어야 하기 때문에 getLottoCountOfResult를 만듦.
    lottoResult => lottoResultCount로 인스턴스 객체 명을 변경했으며,
    Map<LottoResult, Integer>이었던 것을 Map<LottoResult, LottoCount>로 만들었다(primitive 객체 사용 지양)

--- src/main/java/lotto/TotalResult.java
+++ src/main/java/lotto/TotalResult.java
@@ -4,15 +4,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TotalResult {
-    private final Map<LottoResult, Integer> lottoResults = new HashMap<>();
+    private final Map<LottoResult, LottoCount> lottoResultCount = new HashMap<>();
     private Cash totalPrize = new Cash(0);
     private int trial = 0;
 
     public TotalResult(){
-        for (WinningCondition v : WinningCondition.values()){
-            lottoResults.put(v.getLottoResult(), 0);
+        for (int i=0;i<LottoConstants.BALLCOUNT_LIMIT;i++){
+            lottoResultCount.put(new LottoResult(i, false), new LottoCount(0));
+            lottoResultCount.put(new LottoResult(i, true), new LottoCount(0));
         }
+        lottoResultCount.put(new LottoResult(LottoConstants.BALLCOUNT_LIMIT, false), new LottoCount(0));
     }
+    public void add(LottoResult lottoResult) {
+        trial++;
+        addPrize(lottoResult);
+        lottoResultCount.put(lottoResult, lottoResultCount.get(lottoResult).add(1));
+    }
+
     private void addPrize(LottoResult lottoResult){
         List<Cash> prizes = Arrays.stream(WinningCondition.values())
                 .map(v -> v.getPrizeIfMatch(lottoResult))
@@ -22,13 +30,11 @@ public class TotalResult {
             totalPrize = totalPrize.plus(prizes.get(0));
         }
     }
-    public void add(LottoResult lottoResult) {
-        trial++;
-        addPrize(lottoResult);
-        if (lottoResults.containsKey(lottoResult)) {
-            lottoResults.put(lottoResult, lottoResults.get(lottoResult) + 1);
-        }
+
+    public LottoCount getLottoCountOfResult(LottoResult lottoResult){
+        return lottoResultCount.get(lottoResult);
     }
+
     public double getSurplusRatio() throws ArithmeticException{
         if (trial == 0){
             throw new ArithmeticException();
@@ -36,11 +42,19 @@ public class TotalResult {
         return (double)(totalPrize.getCash()) / (trial*LottoConstants.LOTTO_PRICE);
     }
 
-    private String statistics(){
+
+    @Override
+    public String toString(){
+        String startString = "당첨 통계\n-----------\n";
+        return startString + statisticsString() + surplusRatioString();
+    }
+
+
+    private String statisticsString(){
         String[] strings = new String[WinningCondition.values().length];
         for (WinningCondition con : WinningCondition.values()){
             strings[con.getOrder()] = con.toString() + "- " +
-                    lottoResults.get(con.getLottoResult()) + "개";
+                    lottoResultCount.get(con.getLottoResult());
         }
         return String.join("\n", strings)+"\n";
     }
@@ -49,10 +63,5 @@ public class TotalResult {
         double surplusRatio = getSurplusRatio();
         return String.format("총 수익률은 %.2f 입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)\n",
                 surplusRatio, (surplusRatio>1 ? "이득이":"손해"));
-    }
-    @Override
-    public String toString(){
-        String startString = "당첨 통계\n-----------\n";
-        return startString + statistics() + surplusRatioString();
     }
 }
