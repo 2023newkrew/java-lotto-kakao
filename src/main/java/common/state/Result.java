2458efc376c941705ee32b78032c7b8e67be41c0
jerrie.choi <pinkytime96@gmail.com>
2023-01-03 19:24:46 +0900

    feat: Result enum에 설명 문자열 추가

--- src/main/java/common/state/Result.java
+++ src/main/java/common/state/Result.java
@@ -2,15 +2,22 @@ package common.state;
 
 public enum Result {
 
-    NONE(0), THREE(5_000), FOUR(50_000), FIVE(1500_000), FIVEBONUS(30_000_000), SIX(2_000_000_000);
+    NONE(0, ""), THREE(5_000, "3개 일치"), FOUR(50_000, "4개 일치"), FIVE(1500_000, "5개 일치"), FIVEBONUS(30_000_000, "5개 일치, 보너스 볼 일치"), SIX(2_000_000_000, "6개 일치");
 
     private int winnings;
 
-    private Result(int winnings) {
+    private String description;
+
+    private Result(int winnings, String description) {
         this.winnings = winnings;
+        this.description = description;
     }
 
     public int getWinnings() {
         return winnings;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
