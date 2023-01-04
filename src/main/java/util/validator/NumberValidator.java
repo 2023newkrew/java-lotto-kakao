ee4ce095943c59b0ebf64f9b089f96ffcceb17e0
jerrie.choi <pinkytime96@gmail.com>
2023-01-04 21:11:53 +0900

    refac: 숫자 형태 검증 클래스 분리

---
+++ src/main/java/util/validator/NumberValidator.java
@@ -0,0 +1,13 @@
+package util.validator;
+
+public class NumberValidator {
+
+    static void validateNumberFormat(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자가 아닌 문자열이 입력되었습니다.");
+        }
+    }
+
+}
