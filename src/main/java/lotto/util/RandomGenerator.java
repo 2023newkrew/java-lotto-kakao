cfd82aaee6d70e6101774aa04df86c77bfc17e1f
jade.xyz <jade.xyz@kakaocorp.com>
2023-01-04 15:02:24 +0900

    feat(Lotto): RandomGenerator 수정
    sublist를 사용하여 lotto의 수가 모두 동일하게 변경되는 것을 확인하여 새로운 list를 생성하여 반환하도록 수정

--- src/main/java/lotto/util/RandomGenerator.java
+++ src/main/java/lotto/util/RandomGenerator.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomGenerator {
-    public final List<Integer> defaultRange;
+    private final List<Integer> defaultRange;
 
     public RandomGenerator() {
         defaultRange = new ArrayList<>();
@@ -16,15 +16,25 @@ public class RandomGenerator {
 
     public List<Integer> createNumbers(int count) {
         Collections.shuffle(defaultRange);
-        return defaultRange.subList(0, count);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            numbers.add(defaultRange.get(i));
+        }
+
+        return numbers;
     }
 
     public List<Integer> createNumbers(int count, int lowerBound, int upperBound) {
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> range = new ArrayList<>();
         for (int i = lowerBound; i <= upperBound; i++) {
-            numbers.add(i);
+            range.add(i);
         }
-        Collections.shuffle(numbers);
-        return numbers.subList(0, count);
+        Collections.shuffle(range);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            numbers.add(defaultRange.get(i));
+        }
+
+        return numbers;
     }
 }
