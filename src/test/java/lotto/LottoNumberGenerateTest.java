321ae790583db1a256cd5141a142b131aee75552
siryeong <siryeongchoi@gmail.com>
2023-01-04 13:57:13 +0900

    test(lotto number): 무의미한 테스트 삭제
    객체와 관계없는 무의미한 테스트코드 삭제

--- src/test/java/lotto/LottoNumberGenerateTest.java
+++ src/test/java/lotto/LottoNumberGenerateTest.java
@@ -5,35 +5,12 @@ import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottoNumberGenerateTest {
-    @Test
-    @DisplayName("로또는 6개의 숫자로 이루어져 있다.")
-    void lottoHas6Numbers() {
-        assertThat(List.of(1,2,3,4,5,6)).hasSize(6);
-    }
-
-    @Test
-    @DisplayName("로또 번호는 서로 다른 숫자다.")
-    void lottoNumberNotDuplicate() {
-        assertThat(
-                List.of(1,2,3,4,5,6).stream().distinct().count()
-        ).isEqualTo(6);
-    }
-
-    @Test
-    @DisplayName("로또 번호는 1과 45 사이의 숫자로만 이루어져 있다.")
-    void lottoNumberin1To45() {
-        assertThat(
-                List.of(1,2,3,4,5,6).stream().filter(number -> 1 <= number && number <= 45).count()
-        ).isEqualTo(6);
-    }
-
     @Test
     @DisplayName("로또 번호가 6개가 아니면 에러를 던진다.")
     void lottoInvalidNumbersTest1() {
