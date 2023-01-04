a56f6f09401dfd42c3c6f0af6d41cef57ccfef1b
baker.gus <baker.gus@kakaocorp.com>
2023-01-04 14:45:06 +0900

    style: correct spelling

--- src/test/java/lotto/LotteryMatchTest.java
+++ src/test/java/lotto/LotteryMatchTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LotteryMatchTest {
-    @DisplayName("matchCount와 bonusMatch를 입력받아 LotteryMatchf를 생성한다")
+    @DisplayName("matchCount와 bonusMatch를 입력받아 LotteryMatch를 생성한다")
     @Test
     void matchTest() {
         LotteryMatch lotteryMatch = new LotteryMatch(3, true);
