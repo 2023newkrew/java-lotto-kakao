35cad6e9aa4ee6ba85dd2a4fd9ad419f95360b2c
rlfalsgh95 <rlfalsgh95@naver.com>
2023-01-04 14:12:27 +0900

    feat : Lotto 가격 상수 추가

--- src/main/java/domain/LottoConstant.java
+++ src/main/java/domain/LottoConstant.java
@@ -3,5 +3,6 @@ package domain;
 public class LottoConstant {
     public static int LOTTO_NUMBER_MIN_VALUE = 1;
     public static int LOTTO_NUMBER_MAX_VALUE = 45;
+    public static int LOTTO_PRICE = 1_000;
     public static final int LOTTO_LENGTH = 6;
 }
