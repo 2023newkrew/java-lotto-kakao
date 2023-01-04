00fe57127899bf7994eb4f62ea3daae03cb811b9
ekzm8523 <mjw8523@gmail.com>
2023-01-04 20:31:44 +0900

    refactor: static initializer 제거

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,7 +2,10 @@ package lotto.domain;
 
 import static lotto.constant.MessageConstant.INVALID_NUMBER_RANGE;
 
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumber {
@@ -11,18 +14,17 @@ public class LottoNumber {
     static final int MAX_LOTTO_NUMBER = 45;
 
     private final int number;
-    private static final LottoNumber[] CACHED_LOTTO_NUMBER = new LottoNumber[MAX_LOTTO_NUMBER + 1];
 
-    static {
-        IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1)
-                .forEach(i -> CACHED_LOTTO_NUMBER[i] = new LottoNumber(i));
-    }
+    private static final Map<Integer, LottoNumber> CACHED_LOTTO_NUMBER = IntStream
+            .range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1)
+            .boxed()
+            .collect(Collectors.toMap(Function.identity(), LottoNumber::new));
 
     static public LottoNumber from(int number) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
         }
-        return CACHED_LOTTO_NUMBER[number];
+        return CACHED_LOTTO_NUMBER.get(number);
     }
 
     private LottoNumber(int number) {
