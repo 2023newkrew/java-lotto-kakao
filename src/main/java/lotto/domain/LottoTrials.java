0eb98b2fe28e1b6ab30e09b65667af0b7faed9fa
Minsu Kim <ravor8878@gmail.com>
2023-01-04 19:54:46 +0900

    feat : LottoTrials에 iterator 추가

--- src/main/java/lotto/domain/LottoTrials.java
+++ src/main/java/lotto/domain/LottoTrials.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class LottoTrials {
+public class LottoTrials implements Iterator<LottoTrial> {
     private final List<LottoTrial> lottoTrials = new ArrayList<>();
+    private Iterator<LottoTrial> lottoTrialIterator = lottoTrials.iterator();
     public void add(LottoTrial lottoTrial){
         lottoTrials.add(lottoTrial);
     }
@@ -14,5 +16,15 @@ public class LottoTrials {
     }
     public LottoTrial get(int index){
         return lottoTrials.get(index);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return lottoTrialIterator.hasNext();
+    }
+
+    @Override
+    public LottoTrial next() {
+        return lottoTrialIterator.next();
     }
 }
