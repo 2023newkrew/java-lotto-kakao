745f97a221fcbf252a9898bb665898c25b35e097
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-04 13:06:30 +0900

    fix: 어플리케이션이 비정상적(무한루프)으로 동작하는 부분 수정, 재귀 호출 형태로 변환

--- src/main/java/LottoSimulationApplication.java
+++ src/main/java/LottoSimulationApplication.java
@@ -3,12 +3,11 @@ import lotto.controller.LottoController;
 public class LottoSimulationApplication {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        while (true) {
-            try {
-                lottoController.startLottoGame();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            lottoController.startLottoGame();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            main(args);
         }
     }
 }
