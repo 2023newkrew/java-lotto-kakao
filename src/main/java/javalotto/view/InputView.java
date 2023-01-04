1ebc031bdf5ce2e0a32a544756e005f97bec48c9
정핸모 <gusah009@naver.com>
2023-01-04 13:13:54 +0900

    feat: 수동으로 구매할 번호들을 입력받는 기능 추가

--- src/main/java/javalotto/view/InputView.java
+++ src/main/java/javalotto/view/InputView.java
@@ -1,10 +1,8 @@
 package javalotto.view;
 
-import javalotto.domain.Lotto;
-import javalotto.domain.LottoCount;
-import javalotto.domain.PurchaseAmount;
-import javalotto.domain.WinningLotto;
+import javalotto.domain.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -33,6 +31,17 @@ public class InputView {
         return LottoCount.withCount(Integer.parseInt(scanner.nextLine()));
     }
 
+    public Lottos getManuallyLottosInput(LottoCount manuallyLottoCount) {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        List<Lotto> manuallyPurchasedLottos = new ArrayList<>();
+        for (int i = 0; i < manuallyLottoCount.getCount(); i++) {
+            List<Integer> lottoNumbers = getNumbersInput();
+            manuallyPurchasedLottos.add(Lotto.from(lottoNumbers));
+        }
+        return Lottos.from(manuallyPurchasedLottos);
+    }
+
     public WinningLotto getWinningLottoInput() {
         List<Integer> winningNumbers = getWinningNumbersInput();
         int bonusNumber = getBonusNumberInput();
@@ -42,8 +51,15 @@ public class InputView {
 
     private List<Integer> getWinningNumbersInput() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String inputNumbers = scanner.nextLine();
+        return getNumbersInput();
+    }
 
+    private List<Integer> getNumbersInput() {
+        String inputNumbers = scanner.nextLine();
+        return getNumberListBy(inputNumbers);
+    }
+
+    private static List<Integer> getNumberListBy(String inputNumbers) {
         return Arrays.stream(inputNumbers.split(", "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
