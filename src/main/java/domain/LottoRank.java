5600fbb8a86e64bdfa878060b1f87125ac3670ce
rlfalsgh95 <rlfalsgh95@naver.com>
2023-01-04 13:53:07 +0900

    feat : LottoRank의 Rank 순서 변경

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,12 +3,12 @@ package domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6,false,  2_000_000_000),
-    SECOND(5, true, 30_000_000),
+    DEFAULT(0, false, 0),
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
     THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 5_000),
-    FIFTH(3, false, 50_000),
-    DEFAULT(0, false, 0);
+    SECOND(5, true, 30_000_000),
+    FIRST(6,false,  2_000_000_000);
 
     private final int matchCount;
     private final int prizeMoney;
