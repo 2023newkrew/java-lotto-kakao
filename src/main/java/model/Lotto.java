504f3d5396b4a13b7ac3447869f4535a5c0ef0f8
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-04 17:32:21 +0900

    refactor: 이름 변경

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -2,24 +2,25 @@
  * 로또 번호를 감싸는 일급콜렉션
  * 로또 번호는 6개 이다
  * 로또 번호에는 중복이 없다
+ * 불변 객체로 만들 것
  */
 package model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoLine {
+public class Lotto {
 
     private final List<LottoNumber> lottoNumbers;
 
-    public LottoLine(final List<LottoNumber> lottoNumbers) {
-        this.lottoNumbers = new ArrayList<>(lottoNumbers);
+    public Lotto(final List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = new ArrayList<>(lottoNumbers); //방어적 복사.. 인자로 들어온 리스트랑 서로 다른 주소를 가지도록..
     }
-/*
+
     public List<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
     }
-
+/*
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
