d7d23d71f74bda7dcdf1952c38585118868cec1d
ekzm8523 <mjw8523@gmail.com>
2023-01-04 14:39:55 +0900

    refactor: LottoGradeCounter 클래스를 추가함으로써 코드 다이어트

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,14 +1,8 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
-import lotto.LottoGradeEnum;
 import lotto.dto.GameResultDto;
-import lotto.dto.LottoResult;
 
 public class LottoGame {
 
@@ -25,32 +19,16 @@ public class LottoGame {
     }
 
     public GameResultDto getResult() {
-        List<LottoResult> lottoResults = new ArrayList<>();
-        HashMap<LottoGradeEnum, Integer> lottoResultCounter = getLottoResultCounter();
-        int totalPrice = 0;
-        for (Entry<LottoGradeEnum, Integer> entry : lottoResultCounter.entrySet()) {
-            lottoResults.add(new LottoResult(entry.getKey(), entry.getValue()));
-            totalPrice += entry.getKey().price * entry.getValue();
-        }
-        Collections.sort(lottoResults);
-        float rate = (float) totalPrice / (lottos.size() * 1000);
-        return new GameResultDto(lottoResults, rate);
+        LottoGradeCounter lottoGradeCounter = getLottoGradeCounter();
+        float rateOfReturn = (float) lottoGradeCounter.getTotalPrice() /
+                (lottos.size() * LottoGenerator.LOTTO_PRICE);
+        return new GameResultDto(lottoGradeCounter.getLottoGradeCountResults(), rateOfReturn);
     }
 
-    private HashMap<LottoGradeEnum, Integer> getLottoResultCounter() {
-        HashMap<LottoGradeEnum, Integer> lottoResultCounter = new HashMap<>();
-        Arrays.stream(LottoGradeEnum.values())
-                .forEach(lottoGradeEnum -> lottoResultCounter.put(lottoGradeEnum, 0));
-        lottoResultCounter.remove(LottoGradeEnum.NONE_GRADE);
-        lottos.forEach(lotto -> putGrade(lottoResultCounter, winningLotto.getGrade(lotto)));
-        return lottoResultCounter;
-    }
-
-    private static void putGrade(HashMap<LottoGradeEnum, Integer> lottoResultCounter,
-            LottoGradeEnum grade) {
-        if (lottoResultCounter.containsKey(grade)) {
-            lottoResultCounter.put(grade, lottoResultCounter.get(grade) + 1);
-        }
+    private LottoGradeCounter getLottoGradeCounter() {
+        LottoGradeCounter lottoGradeCounter = new LottoGradeCounter();
+        lottos.forEach(lotto -> lottoGradeCounter.putGrade(winningLotto.getGrade(lotto)));
+        return lottoGradeCounter;
     }
 
 }
