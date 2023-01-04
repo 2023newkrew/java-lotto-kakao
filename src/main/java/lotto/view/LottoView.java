aa2c2ed107ad18547174999b33b5687acd360f29
_Min <secured8372@gmail.com>
2023-01-04 17:12:41 +0900

    refactor: <제한자> <static> 순서 리포맷

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -11,46 +11,46 @@ import lotto.dto.GameResultDto;
 public class LottoView {
     static Scanner scanner = new Scanner(System.in);
 
-    static public int inputInit() {
+    public static int inputInit() {
         printInit();
         return Integer.parseInt(scanner.nextLine());
     }
 
-    static private void printInit() {
+    private static void printInit() {
         System.out.println("구매금액을 입력해 주세요.");
     }
 
-    static public void printLottoList(List<Lotto> lottos) {
+    public static void printLottoList(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         lottos.forEach((lotto) -> System.out.println(lotto.toString()));
         System.out.println();
     }
 
-    static private void printWinningLottoNumbers() {
+    private static void printWinningLottoNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
     }
 
-    static private void printBonusNumber() {
+    private static void printBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
     }
 
-    static private List<Integer> inputWinningLottoNumbers() {
+    private static List<Integer> inputWinningLottoNumbers() {
         printWinningLottoNumbers();
         return Arrays.stream(scanner.nextLine().trim().replace(" ", "").split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
-    static private int inputBonusNumber() {
+    private static int inputBonusNumber() {
         printBonusNumber();
         return scanner.nextInt();
     }
 
-    static public WinningLotto inputWinningLotto() {
+    public static WinningLotto inputWinningLotto() {
         return new WinningLotto(inputWinningLottoNumbers(), inputBonusNumber());
     }
 
-    static public void printResult(GameResultDto gameResultDto) {
+    public static void printResult(GameResultDto gameResultDto) {
         final String bonusMatches = ", 보너스 볼 일치";
         Map<LottoGrade, Integer> lottoResultCounter = gameResultDto.getLottoResultCounter();
 
