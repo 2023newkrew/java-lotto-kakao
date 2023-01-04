e8f2c5bfad7fd07d0b5e5dfd6fc5b87a4bc26a7c
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-04 15:35:23 +0900

    feat : controller 패키지 분리 및 Controller 클래스 Main 클래스로부터 분리 Main 클래스 main 메소드 Controller 클래스 사용

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,40 +1,31 @@
-package lotto;
+package lotto.controller;
 
+import lotto.model.*;
 import lotto.view.Input;
 import lotto.view.Output;
-import lotto.model.*;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Main {
-    public static Cash cash;
-    public static LottoCount lottoCount;
-    public static LottoTrials lottoTrials = new LottoTrials();
-    public static WinNumber winNumber;
-    public static TotalResult totalResult = new TotalResult();
+public class Controller {
+    private static LottoCount lottoCount;
+    private static final LottoTrials lottoTrials = new LottoTrials();
+    private static WinNumber winNumber;
+    private static final TotalResult totalResult = new TotalResult();
 
-    public static void main(String[] args) throws IOException {
-        inputCash();
-        createLotto();
-        inputWinningLotto();
-        processLotto();
-        Output.totalResultOutput(totalResult);
-    }
-
-    private static void inputCash() throws IOException {
+    public static void inputCash() throws IOException {
         Output.startCashOutput();
 
-        cash = new Cash(Input.startCashInput());
+        Cash cash = new Cash(Input.startCashInput());
 
         lottoCount = new LottoCount(cash);
 
         Output.lottoCountOutput(lottoCount);
     }
 
-    private static void createLotto() {
+    public static void createLotto() {
         for (int i = 0; i < lottoCount.getCount(); i++){
             LottoTrial lottoTrial = new LottoTrialRandom(new LottoPickerRandom());
 
@@ -44,7 +35,7 @@ public class Main {
         }
     }
 
-    private static void inputWinningLotto() throws IOException {
+    public static void inputWinningLotto() throws IOException {
         Output.winNumOutput();
 
         List<LottoBallNumber> winNum = Arrays.stream(Input.winNumInput())
@@ -58,9 +49,13 @@ public class Main {
         System.out.println();
     }
 
-    private static void processLotto() {
+    public static void processLotto() {
         for (int i = 0; i < lottoCount.getCount(); i++){
             totalResult.addResult(winNumber.compareLotto(lottoTrials.get(i)));
         }
+    }
+
+    public static void outputResult() {
+        Output.totalResultOutput(totalResult);
     }
 }
---
+++ src/main/java/lotto/controller/Main.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Controller.inputCash();
+        Controller.createLotto();
+        Controller.inputWinningLotto();
+        Controller.processLotto();
+        Controller.outputResult();
+    }
+}
