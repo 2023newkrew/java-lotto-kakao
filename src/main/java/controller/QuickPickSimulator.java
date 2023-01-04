a1e090e78649bacaa65a9321619bcb2f6ff59c0d
rlfalsgh95 <rlfalsgh95@naver.com>
2023-01-04 14:14:22 +0900

    feat : LottoSimulator, QuickPickSimulator 클래스 추가

---
+++ src/main/java/controller/QuickPickSimulator.java
@@ -0,0 +1,45 @@
+package controller;
+
+import domain.*;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class QuickPickSimulator implements LottoSimulator{
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoTicketGenerator lottoTicketGenerator;
+
+    public QuickPickSimulator(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoTicketGenerator = new LottoTicketAutoGenerator();
+    }
+
+    @Override
+    public void play() {
+        List<LottoTicket> purchaseLottoTickets = getPurchaseLottoTickets();
+        outputView.printLottoPurchaseInfo(purchaseLottoTickets);
+
+        List<LottoMatchResult> lottoMatchResults = playLottoGames(purchaseLottoTickets);
+        LottoMatchStatistics lottoMatchStatistics = new LottoMatchStatistics(lottoMatchResults);
+        outputView.printLottoMatchStatistics(lottoMatchStatistics);
+    }
+
+    private List<LottoMatchResult> playLottoGames(List<LottoTicket> purchaseLottoTickets) {
+        WinningLotto lastWinningLotto = inputView.getLastWinningLotto();
+
+        return purchaseLottoTickets.stream()
+                .map(lastWinningLotto::match)
+                .collect(Collectors.toList());
+    }
+
+    private List<LottoTicket> getPurchaseLottoTickets() {
+        int purchasePrice = inputView.getPurchasePrice();
+        int lottoCount =  purchasePrice / LottoConstant.LOTTO_PRICE;
+
+        return lottoTicketGenerator.generate(lottoCount);
+    }
+}
