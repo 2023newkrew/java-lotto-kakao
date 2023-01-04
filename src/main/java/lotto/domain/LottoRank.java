c961dfc0be3c577cb5617661703b5940e97af77c
siryeong <siryeongchoi@gmail.com>
2023-01-04 16:54:01 +0900

    refactor(lotto rank): lotto rank 리팩터링
    lotto rank 리팩터링
    - bonus 정보 추가
    - 번호 일치 개수와 보너스 일치 여부를 통해 Rank를 반환하는 기능 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,18 +1,29 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    FAIL(0, 0);
+    FIRST(6, false,2_000_000_000),
+    SECOND(5, true,30_000_000),
+    THIRD(5, false,1_500_000),
+    FOURTH(4, false,50_000),
+    FIFTH(3, false,5_000),
+    FAIL(0, false,0);
 
     public final int COUNT;
+    public final boolean BONUS;
     public final int PRIZE;
 
-    LottoRank(int count, int prize) {
+    LottoRank(int count, boolean bonus, int prize) {
         this.COUNT = count;
+        this.BONUS = bonus;
         this.PRIZE = prize;
+    }
+
+    public static LottoRank getRank(int count, boolean bonus) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank.COUNT==count && lottoRank.BONUS == bonus)
+                .findAny()
+                .orElse(FAIL);
     }
 }
