8cf0ad65ab56a5d10de0fb30d43084015927e750
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-04 17:00:43 +0900

    feat : WinningCondition enum 개인 코드 컨벤션 최적화

--- src/main/java/lotto/model/WinningCondition.java
+++ src/main/java/lotto/model/WinningCondition.java
@@ -1,19 +1,15 @@
 package lotto.model;
 
-import lotto.model.Cash;
-import lotto.model.LottoConstants;
-import lotto.model.LottoResult;
-
 public enum WinningCondition {
     FIRST_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT, false),
             new Cash(LottoConstants.FIRST_PRIZE_CASH), 4),
-    SECOND_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1, true),
+    SECOND_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT - 1, true),
             new Cash(LottoConstants.SECOND_PRIZE_CASH), 3),
-    THIRD_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-1, false),
+    THIRD_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT - 1, false),
             new Cash(LottoConstants.THIRD_PRIZE_CASH), 2),
-    FOURTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-2, false),
+    FOURTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT - 2, false),
             new Cash(LottoConstants.FOURTH_PRIZE_CASH), 1),
-    FIFTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT-3, false),
+    FIFTH_PRIZE(new LottoResult(LottoConstants.BALLCOUNT_LIMIT - 3, false),
             new Cash(LottoConstants.FIFTH_PRIZE_CASH), 0);
 
     private final LottoResult lottoResult;
