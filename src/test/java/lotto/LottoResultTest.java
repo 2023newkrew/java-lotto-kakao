51ec56249845e584d86ec2449d5c26ec404616ce
abcworld123 <rkaxhdals@gmail.com>
2023-01-04 18:53:45 +0900

    test: 로또 결과 테스트 코드 수정

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -4,55 +4,43 @@ import lotto.domain.*;
 import lotto.util.LottoPayment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 
-
-public class LottoResultTest {
-    @Test
-    @DisplayName("1등 당첨 테스트")
-    void 일등당첨() {
-        Lotto myLotto = new Lotto(List.of(1,2,3,4,5,6));
-        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+class LottoResultTest {
+    @ParameterizedTest
+    @MethodSource("lottoMatchTestGenerator")
+    @DisplayName("1등, 2등, 5등 당첨 테스트")
+    void lottoMatchTest(Lotto myLotto, LottoRank expectedRank) {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         LottoNumber bonusNumber = new LottoNumber(7);
 
         LottoResult result = new LottoResult(myLotto, winningLotto, bonusNumber);
-        assertThat(result.getRank()).isEqualTo(LottoRank.FIRST);
+        assertThat(result.getRank()).isEqualTo(expectedRank);
+    }
+
+    private static Stream<Arguments> lottoMatchTestGenerator() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoRank.FIRST),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)), LottoRank.SECOND),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 8, 9, 10)), LottoRank.FIFTH)
+        );
     }
 
     @Test
-    @DisplayName("2등 당첨 테스트")
-    void 이등당첨() {
-        Lotto myLotto = new Lotto(List.of(1,2,3,4,5,7));
-        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
-        LottoNumber bonusNumber = new LottoNumber(7);
-
-        LottoResult result = new LottoResult(myLotto, winningLotto, bonusNumber);
-        assertThat(result.getRank()).isEqualTo(LottoRank.SECOND);
-    }
-
-    @Test
-    @DisplayName("5등 당첨 테스트")
-    void 오등당첨() {
-        Lotto myLotto = new Lotto(List.of(1,2,3,8,9,10));
-        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
-        LottoNumber bonusNumber = new LottoNumber(7);
-
-        LottoResult result = new LottoResult(myLotto, winningLotto, bonusNumber);
-        assertThat(result.getRank()).isEqualTo(LottoRank.FIFTH);
-    }
-
-    @Test
-    @DisplayName("여러개의 로또 당첨시 당첨 개수 제공")
-    void 일등한개이등한개오등한개개수() {
+    @DisplayName("로또 등수별 당첨 통계 테스트")
+    void lottoStatisticsTest() {
         Statistics stat = new Statistics();
         stat.add(LottoRank.FIRST);
         stat.add(LottoRank.SECOND);
         stat.add(LottoRank.FIFTH);
-
         assertThat(stat.getByRank(LottoRank.FIRST)).isEqualTo(1);
         assertThat(stat.getByRank(LottoRank.SECOND)).isEqualTo(1);
         assertThat(stat.getByRank(LottoRank.THIRD)).isEqualTo(0);
@@ -61,25 +49,15 @@ public class LottoResultTest {
     }
 
     @Test
-    @DisplayName("상금 일치 테스트")
-    void 일등한개이등한개오등한개상금() {
-        Statistics stat = new Statistics();
-        stat.add(LottoRank.FIRST);
-        stat.add(LottoRank.SECOND);
-        stat.add(LottoRank.FIFTH);
-        int totalPrize = (LottoRank.FIRST.PRIZE + LottoRank.SECOND.PRIZE + LottoRank.FIFTH.PRIZE);
-        assertThat(stat.getPrizeAmount()).isEqualTo(totalPrize);
-    }
-
-    @Test
-    @DisplayName("수익률 일치 테스트")
-    void profitRateTest() {
+    @DisplayName("상금, 수익률 일치 테스트")
+    void lottoPrizeTest() {
         Statistics stat = new Statistics();
         stat.add(LottoRank.FIRST);
         stat.add(LottoRank.SECOND);
         stat.add(LottoRank.FIFTH);
         int totalPrize = (LottoRank.FIRST.PRIZE + LottoRank.SECOND.PRIZE + LottoRank.FIFTH.PRIZE);
         double profitRate = totalPrize / (double) (3 * LottoPayment.LOTTO_COST);
+        assertThat(stat.getPrizeAmount()).isEqualTo(totalPrize);
         assertThat(stat.getProfitRate()).isCloseTo(profitRate, within(0.0001));
     }
 }
