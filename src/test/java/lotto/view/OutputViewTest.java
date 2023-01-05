67703b21fded45da3eaad09bce58b789e9ccc620
gh1224 <rlarkgus0@naver.com>
2023-01-05 22:38:00 +0900

    test: 출력 테스트 System.setOut() 메소드 사용

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,25 +1,50 @@
 package lotto.view;
 
 import lotto.*;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class OutputViewTest {
+    private final PrintStream standardOut = System.out;
+    private final OutputStream out = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
     @Test
     @DisplayName("로또 출력")
     void printLottoTickets() {
-        RandomLottoTicketGenerator randomLottoTicketGenerator = new RandomLottoTicketGenerator();
         List<LottoTicket> lottoTickets = List.of(
-                randomLottoTicketGenerator.generate(),
-                randomLottoTicketGenerator.generate(),
-                randomLottoTicketGenerator.generate()
+                new LottoTicket(List.of(
+                        new LottoBall(1),
+                        new LottoBall(2),
+                        new LottoBall(3),
+                        new LottoBall(4),
+                        new LottoBall(5),
+                        new LottoBall(6)
+                ))
         );
 
         OutputView outputView = new OutputView();
-        outputView.printLottoTickets(lottoTickets, 0);
+        outputView.printLottoTickets(lottoTickets, 1);
+
+        String output = "수동으로 1장, 자동으로 0개를 구매했습니다.\\n" +
+                "\\[1, 2, 3, 4, 5, 6\\]\\n";
+        assertThat(out.toString()).matches(output);
     }
 
     @Test
@@ -34,8 +59,14 @@ class OutputViewTest {
         OutputView outputView = new OutputView();
         outputView.printMatchResult(matchResult);
 
-        // 구입한 로또: 4개(4000원)
-        // 당첨금: 5000원
-        // 수익률: 1.25
+        String output = "당첨 통계\\n" +
+                "----------\\n" +
+                "3개 일치 \\(5000원\\)- 1개\\n" +
+                "4개 일치 \\(50000원\\)- 0개\\n" +
+                "5개 일치 \\(1500000원\\)- 0개\\n" +
+                "5개 일치, 보너스 볼 일치 \\(30000000원\\)- 0개\\n" +
+                "6개 일치 \\(2000000000원\\)- 0개\\n" +
+                "총 수익률은 1.25[0]*입니다.\\n";
+        assertThat(out.toString()).matches(output);
     }
 }
