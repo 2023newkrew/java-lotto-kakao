c02a79584126a7d6e4dfbb301a5fb6ea2313f419
baker.gus <baker.gus@kakaocorp.com>
2023-01-05 18:07:54 +0900

    feat: 숫자 입력 예외 추가

--- src/main/java/util/StringParser.java
+++ src/main/java/util/StringParser.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringParser {
-    public static List<Integer> parse(String text){
-        String[] texts = text.split(", ");
+    public static List<Integer> parse(String text) {
+        String[] texts = text.split(", ?");
 
         return Arrays.stream(texts).map(Integer::parseInt).collect(Collectors.toList());
     }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import domain.lotto.LotteryNumbers;
 import domain.lotto.LotteryResult;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,54 +13,63 @@ public class InputView {
 
     public int getBudgetInput() {
         System.out.println("구입금액을 입력해 주세요.");
-        int budget = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println();
 
-        return budget;
+        return getNumberInput();
     }
 
-    public LotteryResult getLotteryResult() {
-        LotteryResult lotteryResult = new LotteryResult(getWinningNumbersInput(), getBonusNumberInput());
-        System.out.println();
+    private int getNumberInput() {
+        try {
+            int result = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
 
-        return lotteryResult;
+            return result;
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요");
+        }
+    }
+
+
+    public LotteryResult getLotteryResult() {
+        return new LotteryResult(getWinningNumbersInput(), getBonusNumberInput());
     }
 
     private LotteryNumbers getWinningNumbersInput() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return new LotteryNumbers(scanner.nextLine());
+
+        return getLotteryNumbersInput();
     }
 
     private int getBonusNumberInput() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return scanner.nextInt();
+        return getNumberInput();
     }
 
     public int getManualCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해주세요");
-        int manualCount = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println();
 
-        return manualCount;
+        return getNumberInput();
     }
 
-    public List<LotteryNumbers> getManualLotteries(int cnt) {
-        if (cnt == 0) return new ArrayList<>();
+    public List<LotteryNumbers> getManualLotteries(int count) {
+        if (count == 0) return new ArrayList<>();
 
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
 
         List<LotteryNumbers> result = new ArrayList<>();
-        for (int i = 0; i < cnt; ++i) {
-            result.add(new LotteryNumbers(getManualNumbers()));
+        for (int i = 0; i < count; ++i) {
+            result.add(getLotteryNumbersInput());
         }
         System.out.println();
 
         return result;
     }
 
-    public String getManualNumbers() {
-        return scanner.nextLine();
+    private LotteryNumbers getLotteryNumbersInput() {
+        try {
+            return new LotteryNumbers(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 반점으로 구분해 입력해주세요");
+        }
     }
 }
