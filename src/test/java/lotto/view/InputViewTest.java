111ab3dacab6eca02644061772f1c4dd3c6c38f3
gh1224 <rlarkgus0@naver.com>
2023-01-05 20:30:26 +0900

    test: 수동 로또 관련 입력 테스트 추가

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -35,6 +35,23 @@ class InputViewTest {
         assertThat(bonusNumber).isEqualTo(7);
     }
 
+    @Test
+    @DisplayName("수동 로또 개수 입력")
+    void scanNumberOfManualLotto() {
+        InputView inputView = new InputView(createScanner("3"));
+        int numberOfManualLotto = inputView.scanNumberOfManualLotto();
+        assertThat(numberOfManualLotto).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("수동 로또 번호 입력")
+    void scanManualLottoNumbers() {
+        InputView inputView = new InputView(createScanner("1,2,3,4,5,6\n7,8,9,10,11,12"));
+        List<List<Integer>> manualLottoNumbers = inputView.scanManualLottoNumbers(2);
+        assertThat(manualLottoNumbers.get(0)).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(manualLottoNumbers.get(1)).containsExactly(7, 8, 9, 10, 11, 12);
+    }
+
     private static Scanner createScanner(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         Scanner sc = new Scanner(in);
