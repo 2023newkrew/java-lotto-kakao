50ce30142250c86996ae4edbceb3dd196e595f63
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-05 13:54:42 +0900

    feat: LottoCount 클래스 생성

---
+++ src/main/java/domain/LottoCount.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class LottoCount {
+    private final int manualLottoCount;
+    private final int automaticLottoCount;
+    public LottoCount(int totalLottoCount, int desiredLottoCount) {
+        if(totalLottoCount<desiredLottoCount){
+            throw new IllegalArgumentException("수동 구매하려는 로또의 수가 총 구매 가능 로또 수를 초과합니다.");
+        }
+        this.manualLottoCount = desiredLottoCount;
+        this.automaticLottoCount = totalLottoCount - desiredLottoCount;
+    }
+}
