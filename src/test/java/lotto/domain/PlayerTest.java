289cdcd38d657b608b083da3b2f2a7808ceaaa9f
joelonsw <joel610naver@gmail.com>
2023-01-05 15:29:10 +0900

    test(PlayerTest): PlayerTest 테스트 보강
    - 수동 구입에 대한 테스트 작성

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -18,9 +20,9 @@ public class PlayerTest {
         assertThatThrownBy(() -> new Player(invalidMoney));
     }
 
-    @DisplayName("플레이어는 로또 티켓을 구매할 수 있다")
+    @DisplayName("플레이어는 자동 로또 티켓을 구매할 수 있다")
     @Test
-    void buyLottoTicket() {
+    void buyAutoLottoTicket() {
         //given
         Player player = new Player(5_500);
         LottoTicketSeller seller = new LottoTicketSeller();
@@ -29,14 +31,28 @@ public class PlayerTest {
         assertDoesNotThrow(() -> player.buyAutoLottoTickets(seller));
     }
 
+    @DisplayName("플레이어는 수동으로 로또 티켓을 구매할 수 있다.")
+    @Test
+    void buyManualLottoTicket() {
+        //given
+        Player player = new Player(5_500);
+        LottoTicketSeller seller = new LottoTicketSeller();
+        List<List<Integer>> lottoNumbers = List.of(List.of(1, 2, 3, 4, 5, 6));
+
+        //when & then
+        assertDoesNotThrow(() -> player.buyManualLottoTickets(seller, lottoNumbers));
+    }
+
     @DisplayName("플레이어는 자신의 로또 티켓들의 당첨 결과를 받을 수 있다")
     @Test
     void calculateResults() {
         //given
         Player player = new Player(5_500);
         LottoTicketSeller seller = new LottoTicketSeller();
+        List<List<Integer>> lottoNumbers = List.of(List.of(1, 2, 3, 4, 5, 6));
 
         //when
+        player.buyManualLottoTickets(seller, lottoNumbers);
         player.buyAutoLottoTickets(seller);
         PlayerLottoResult playerLottoResult = player.findResult(lottoTicket -> LottoResult.FIFTH_PLACE);
 
