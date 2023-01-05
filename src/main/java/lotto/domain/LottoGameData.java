12b94123fbe09f715c3883bc7505a2c719918063
Neurumaru <ksj6659@gmail.com>
2023-01-05 17:10:21 +0900

    fix(LottoGameData): 이동된 데이터에 대한 삭제

--- src/main/java/lotto/domain/LottoGameData.java
+++ src/main/java/lotto/domain/LottoGameData.java
@@ -1,28 +1,11 @@
 package lotto.domain;
 
 public class LottoGameData {
+
     private int leftoverMoney = 0;
-    private int manualTicketCount = 0;
-    private int randomTicketCount = 0;
-
-    public void addManualTicketCount(int count) {
-        manualTicketCount += count;
-    }
-
-    public void addRandomTicketCount(int count) {
-        randomTicketCount += count;
-    }
 
     public void saveLeftoverMoney(int leftoverMoney) {
         this.leftoverMoney += leftoverMoney;
-    }
-
-    public int getManualTicketCount() {
-        return manualTicketCount;
-    }
-
-    public int getRandomTicketCount() {
-        return randomTicketCount;
     }
 
     public int receiveLeftoverMoney() {
