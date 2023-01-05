cce701b30c5fc3c2e5262e12411636c71de65d08
jerrie.choi <pinkytime96@gmail.com>
2023-01-05 17:56:25 +0900

    refac: Count  클래스 테스트 코드 추가

--- src/test/java/domain/CountTest.java
+++ src/test/java/domain/CountTest.java
@@ -3,9 +3,11 @@ package domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -51,6 +53,31 @@ public class CountTest {
     @ValueSource(strings = {"1", "3", "5", "7", "9"})
     void createCount(String countUnderTotalCount) {
         Assertions.assertDoesNotThrow(() -> new Count(countUnderTotalCount, totalCount));
+    }
+
+    @DisplayName("0인지 여부 판별 참 반환 테스트")
+    @Test
+    void zeroTest() {
+        String zero = "0";
+        Count zeroCount = new Count(zero, totalCount);
+        assertThat(zeroCount.zero()).isTrue();
+    }
+
+    @DisplayName("0인지 여부 판별 거짓 반환 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    void nonZeroTest(String nonZero) {
+        Count zeroCount = new Count(nonZero, totalCount);
+        assertThat(zeroCount.zero()).isFalse();
+    }
+
+    @DisplayName("나머지 반환 테스트")
+    @Test
+    void getRemainsTest() {
+        String three = "3";
+        int remains = 7;
+        Count count = new Count(three, totalCount);
+        assertThat(count.getRemains()).isEqualTo(remains);
     }
 
 }
