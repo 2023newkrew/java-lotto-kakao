18f6f24537f298c76cda803d2e2e8c0feecdf13a
rlfalsgh95 <rlfalsgh95@naver.com>
2023-01-05 11:50:50 +0900

    feat(InputView) : 입력 validation check 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -22,6 +22,9 @@ public class InputView {
     public int getPurchasePrice() {
         outputStream.println("구입금액을 입력해 주세요.");
         String purchasePrice = inputStream.nextLine();
+        if(!isInteger(purchasePrice)){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
         return Integer.parseInt(purchasePrice);
     }
 
@@ -36,8 +39,35 @@ public class InputView {
         outputStream.println();
         outputStream.println("지난 주 당첨 번호를 입력해 주세요.");
         String lottoNumbers = inputStream.nextLine();
+        List<String> splitNumbers = trim(split(lottoNumbers));
 
-        return toInteger(split(lottoNumbers));
+        if(!isPositiveIntegers(splitNumbers)){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        return toInteger(splitNumbers);
+    }
+
+    private LottoNumber getLastBonusNumber(){
+        outputStream.println("보너스 볼을 입력해 주세요.");
+        String bonusLottoNumber = inputStream.nextLine();
+        if(!isInteger(bonusLottoNumber)){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        return new LottoNumber(Integer.parseInt(bonusLottoNumber));
+    }
+
+    private boolean isPositiveIntegers(List<String> texts){
+        return texts.stream()
+                .allMatch(this::isInteger);
+    }
+
+    private boolean isInteger(String text){
+        try{
+            Integer.parseInt(text);
+            return true;
+        }catch (NumberFormatException numberFormatException){
+            return false;
+        }
     }
 
     private List<Integer> toInteger(List<String> numbers){
@@ -47,13 +77,13 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
-    private List<String> split(String numbers){
-        return List.of(numbers.split(","));
+    private List<String> trim(List<String> numbers){
+        return numbers.stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
-    private LottoNumber getLastBonusNumber(){
-        outputStream.println("보너스 볼을 입력해 주세요.");
-        String bonusLottoNumber = inputStream.nextLine();
-        return new LottoNumber(Integer.parseInt(bonusLottoNumber));
+    private List<String> split(String numbers){
+        return List.of(numbers.split(","));
     }
 }
