d3d9d2830134343745dc862a1765a08b14e395d0
Subin Min <tnqls5417@naver.com>
2023-01-05 14:50:33 +0900

    style: 주석 제거

--- src/main/java/controller/LottoApplication.java
+++ src/main/java/controller/LottoApplication.java
@@ -22,10 +22,8 @@ public class LottoApplication {
         LottoStore lottoStore = new LottoStore();
 
         try {
-            // 구입금액 입력
             Integer money = inputView.getMoneyInput();
 
-            // 수동 구매 수 입력
             Integer amount = inputView.getManualAmountInput();
 
             List<Lotto> manualLottos = inputView.getManualLottosInput(amount)
@@ -33,19 +31,14 @@ public class LottoApplication {
                     .map(Lotto::ofManual)
                     .collect(Collectors.toList());
 
-            // 수동 구매 (입력) + 자동 구매 (남은 금액) 로또 구매
             LottoBuyer lottoBuyer = new LottoBuyer(money);
             PurchasedLotto purchasedLotto = lottoBuyer.buyFrom(lottoStore, manualLottos);
             outputView.printPurchasedLottos(purchasedLotto);
 
-            // 당첨번호 입력
             Lotto winningLotto = Lotto.ofManual(inputView.getWinningLottoInput());
-
-            // 보너스 번호 입력
             LottoNumber bonusNumber = new LottoNumber(inputView.getBonusNumberInput());
             inputView.close();
 
-            // 로또 당첨 결과 출력
             LottoResult lottoResult = lottoBuyer.calculateResult(new WinningLotto(winningLotto, bonusNumber));
             outputView.printLottoStatistics(lottoResult);
         }
