78d7d5fc31b52ae7fce76799f9607ba75a1ce021
HQkim <hyunkyu001@gmail.com>
2023-01-05 15:03:44 +0900

    chore: LottoController 주석 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -13,26 +13,31 @@ public class LottoController {
 
     public void play() {
         try {
+            // 구매할 로또 정보 입력
             int expenseInput = inputView.getExpenseInput();
             int totalAmount = LottoSeller.getLottoAmount(new Money(expenseInput));
             int manualAmount = inputView.getManualAmount();
 
+            // 구매 로또 생성
             List<List<Integer>> manualNumbersGroup = inputView.getManualLottosInput(manualAmount);
-
             UserLottoTicket userLottoTicket = new UserLottoTicket(
                     LottoSeller.generateRandomLottos(totalAmount - manualAmount),
                     LottoSeller.generateManualLottos(manualNumbersGroup));
 
+            // 구매 정보 출력
             outputView.printPurchaseAmount(totalAmount, manualAmount);
             outputView.printUserLottos(userLottoTicket.manualLottoTicket());
             outputView.printUserLottos(userLottoTicket.randomLottoTicket());
 
+            // 정답 로또 입력 및 생성
             List<Integer> lottoNumbers = inputView.getAnswerLottoInput();
             LottoNumber bonusNumber = LottoNumber.from(inputView.getBonusBallInput());
             List<LottoNumber> answerLottoNumbers = lottoNumbers.stream()
                     .map(LottoNumber::from)
                     .collect(Collectors.toList());
             WinningLotto winningLotto = new WinningLotto(new LottoNumbers(answerLottoNumbers), bonusNumber);
+
+            // 로또 결과 출력
             outputView.printResult(userLottoTicket.getLottoPrizeCountMap(winningLotto));
         } catch (Exception e) {
             System.out.println(e.getMessage());
