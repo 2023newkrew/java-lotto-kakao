d9371e2ee887d1323b5e6c85be28ac1291c9121b
yooj929 <jang0929@naver.com>
2023-01-05 14:52:16 +0900

    refactor: LottoRank 안쓰는 변수 및 메소드 제거

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,21 +4,19 @@ import java.util.Arrays;
 
 public enum LottoRank {
 
-    FIRST(1, 6, false, 2_000_000_000),
-    SECOND(2, 5, true, 30_000_000),
-    THIRD(3, 5, false, 1_500_000),
-    FOURTH(4, 4, false, 50_000),
-    FIFTH(5, 3, false, 5_000),
-    NOTHING(6, 2, false, 0),
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NOTHING(2, false, 0),
     ;
 
-    private final int rank;
     private final int matchCount;
     private final boolean requiredBonus;
     private final long winning;
 
-    LottoRank(int rank, int matchCount, boolean requiredBonus, long winning) {
-        this.rank = rank;
+    LottoRank(int matchCount, boolean requiredBonus, long winning) {
         this.matchCount = matchCount;
         this.requiredBonus = requiredBonus;
         this.winning = winning;
@@ -37,7 +35,7 @@ public enum LottoRank {
         if (lottoRank.requiredBonus) {
             return requiredBonusString(lottoRank, count);
         }
-        if(lottoRank == NOTHING){
+        if (lottoRank == NOTHING) {
             return "";
         }
         return String.format("%d개 일치 (%d원)- %d개", lottoRank.matchCount, lottoRank.winning, count);
@@ -45,18 +43,6 @@ public enum LottoRank {
 
     private static String requiredBonusString(LottoRank lottoRank, int count) {
         return String.format("%d개 일치, 보너스 볼 일치(%d원) - %d개", lottoRank.matchCount, lottoRank.winning, count);
-    }
-
-    public int getRank() {
-        return rank;
-    }
-
-    public int getMatchCount() {
-        return matchCount;
-    }
-
-    public boolean isRequiredBonus() {
-        return requiredBonus;
     }
 
     public long getWinning() {
