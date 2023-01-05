8dfef14fb4b329f3d036b4fbeea8e9deb6932d82
wilgur513 <wilgur513@naver.com>
2023-01-05 15:18:45 +0900

    test: LottoNumber 관련 테스트 추가

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
-import lotto.model.LottoNumber;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,5 +18,26 @@ class LottoNumberTest {
         Assertions.assertThatThrownBy(() -> new LottoNumber(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호는 1 ~ 45 사이의 숫자입니다.");
+    }
+
+    @DisplayName("로또 번호를 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 13, 34, 45})
+    void createValidLottoNumber(int number) {
+        Assertions.assertThatCode(() -> new LottoNumber(number))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 번호 풀은 1~45의 숫자를 한개씩 가지고 있다.")
+    @Test
+    void createPool() {
+        List<LottoNumber> actual = LottoNumber.createPool();
+
+        List<LottoNumber> expected = IntStream.rangeClosed(1, 45)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(actual)
+                .containsExactlyInAnyOrderElementsOf(expected);
     }
 }
