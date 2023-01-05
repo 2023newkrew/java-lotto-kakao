280eddd609752b8aeeeb1ac96946ce036b25919b
kyne.k <dd2307@snu.ac.kr>
2023-01-05 17:38:02 +0900

    feat: InputView 구현
    - InputView 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,58 @@
 package lotto.view;
 
+import static lotto.constant.ExceptionMessage.INPUT_BUDGET_WRONG_TYPE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
+    private static final String INPUT_SELF_PICK_COUNT_WRONG_TYPE = "";
+    private static final String INPUT_LOTTERY_NUMBER_WRONG_TYPE = "";
+    public static final String LOTTERY_NUMBERS_DELIMITER = ",";
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int readBudget() {
+        String input = scanner.nextLine();
+        return parseInt(input, INPUT_BUDGET_WRONG_TYPE);
+    }
+
+    public int readSelfPickCount() {
+        String input = scanner.nextLine();
+        return parseInt(input, INPUT_SELF_PICK_COUNT_WRONG_TYPE);
+    }
+
+    public List<List<Integer>> readSelfPickNumbers(int selfPickCount) {
+        List<List<Integer>> selfPickNumbers = new ArrayList<>();
+        for (int index = 0;index < selfPickCount;index++) {
+            selfPickNumbers.add(readLottoNumberCombination());
+        }
+        return selfPickNumbers;
+    }
+
+    public List<Integer> readWinningLotteryNumberCombination() {
+        return readLottoNumberCombination();
+    }
+
+    public int readBonusNumber() {
+        String input = scanner.nextLine();
+        return parseInt(input, INPUT_LOTTERY_NUMBER_WRONG_TYPE);
+    }
+
+    private List<Integer> readLottoNumberCombination() {
+        String input = scanner.nextLine();
+        return Arrays.stream(input.split(LOTTERY_NUMBERS_DELIMITER))
+                .map(x -> parseInt(x, INPUT_LOTTERY_NUMBER_WRONG_TYPE))
+                .collect(Collectors.toList());
+    }
+
+    private int parseInt(String input, String exceptionMessage) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(exceptionMessage, e);
+        }
+    }
 }
