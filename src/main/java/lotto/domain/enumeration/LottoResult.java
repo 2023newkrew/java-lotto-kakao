366a2a925d78932e8cbd711acd46a9ce8e50ce54
jayden.v <jayden.v@kakaocorp.com>
2023-01-05 16:23:40 +0900

    fix: 등수 계산 로직의 오류 수정
    - 보너스 번호를 맞춘 경우 4등, 5등을 받지 못 했던 오류 수정
    - 이 문제와 관련한 테스트케이스 추가

--- src/main/java/lotto/domain/enumeration/LottoResult.java
+++ src/main/java/lotto/domain/enumeration/LottoResult.java
@@ -1,32 +1,30 @@
 package lotto.domain.enumeration;
 
-import java.util.Arrays;
-
 public enum LottoResult {
-    FIFTH(3, false, 5000, "%d개 일치 (%d원)"),
-    FOURTH(4, false, 50000, "%d개 일치 (%d원)"),
-    THIRD(5, false, 1500000, "%d개 일치 (%d원)"),
-    SECOND(5, true, 30000000, "%d개 일치, 보너스 볼 일치(%d원)"),
-    FIRST(6, false, 2000000000, "%d개 일치 (%d원)"),
-    MISS(0, false, 0, "꽝");
+    FIFTH(3,5000, "%d개 일치 (%d원)"),
+    FOURTH(4,50000, "%d개 일치 (%d원)"),
+    THIRD(5,1500000, "%d개 일치 (%d원)"),
+    SECOND(5,30000000, "%d개 일치, 보너스 볼 일치(%d원)"),
+    FIRST(6,2000000000, "%d개 일치 (%d원)"),
+    MISS(0,0, "꽝");
 
     private final int numberCount;
-    private final boolean bonusNumber;
     private final int prize;
     private final String stringFormat;
 
-    LottoResult(int numberCount, boolean bonusNumber, int prize, String stringFormat) {
+    LottoResult(int numberCount, int prize, String stringFormat) {
         this.numberCount = numberCount;
-        this.bonusNumber = bonusNumber;
         this.prize = prize;
         this.stringFormat = stringFormat;
     }
 
     public static LottoResult getLottoResultOf(int numberCount, boolean hasBonusNumber) {
-        return Arrays.stream(values())
-                .filter(it -> it.numberCount == numberCount)
-                .filter(it -> it.bonusNumber == hasBonusNumber)
-                .findFirst().orElse(MISS);
+        if (numberCount == 6) { return FIRST; }
+        if (numberCount == 5 && hasBonusNumber) { return SECOND; }
+        if (numberCount == 5 ) { return THIRD; }
+        if (numberCount == 4) { return FOURTH; }
+        if (numberCount == 3) { return FIFTH; }
+        return MISS;
     }
 
     public String getString() {
