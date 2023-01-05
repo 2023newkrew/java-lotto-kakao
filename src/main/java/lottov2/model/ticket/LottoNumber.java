6bf75d4572b8e1258ff8af6537eadf216cf1a317
pluto.yun <pluto.yun@kakaocorp.com>
2023-01-05 12:00:51 +0900

    feat(v2): LottoNumber 추가

--- src/main/java/lottov2/model/ticket/LottoNumber.java
+++ src/main/java/lottov2/model/ticket/LottoNumber.java
@@ -1,5 +1,32 @@
 package lottov2.model.ticket;
 
 public class LottoNumber {
+
+    private static final int MIN_NUMBER = 1;
+
+    private static final int MAX_NUMBER = 45;
+
+    private final int value;
+
+    public static LottoNumber valueOf(int value) {
+        if (isOutOfRange(value)) {
+            String errorMessage = String.format("로또 번호는 %d ~ %d 사이의 숫자입니다.", MIN_NUMBER, MAX_NUMBER);
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return new LottoNumber(value);
+    }
+
+    private LottoNumber(int value) {
+        this.value = value;
+    }
+
+    private static boolean isOutOfRange(int value) {
+        return value < MIN_NUMBER || value > MAX_NUMBER;
+    }
+
+    public int intValue() {
+        return value;
+    }
 }
 
