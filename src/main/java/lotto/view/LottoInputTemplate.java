821bbd2bab5d6a4f2f105348b9fb25fa1f2c1560
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-05 13:43:48 +0900

    feat: 수동으로 구매할 로또의 수와 번호를 입력받는 코드 추가

--- src/main/java/lotto/view/LottoInputTemplate.java
+++ src/main/java/lotto/view/LottoInputTemplate.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class LottoInputTemplate {
     private final Scanner scanner;
@@ -16,6 +19,19 @@ public class LottoInputTemplate {
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return getNextInputLine();
+    }
+
+    public String inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return getNextInputLine();
+    }
+
+    public List<String> inputManualLottos(Integer manualLottoCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> manualLottos = new ArrayList<>();
+        IntStream.range(0, manualLottoCount)
+                .forEach(i -> manualLottos.add(getNextInputLine()));
+        return manualLottos;
     }
 
     public String inputLottoWinningNumbers() {
