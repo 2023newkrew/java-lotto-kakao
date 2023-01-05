73fa23049344fb62a4069dcb16362ef1a6c4936c
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-05 17:57:28 +0900

    style

--- src/test/java/LottoNumberTest.java
+++ src/test/java/LottoNumberTest.java
@@ -6,16 +6,17 @@ import org.junit.jupiter.api.Test;
 public class LottoNumberTest {
     @Test
     void 로또_번호는_1이상_45이하의_정수이다() {
-        for(int i = 1; i<=45; i++)
+        for (int i = 1; i <= 45; i++)
             Assertions.assertTrue(LottoNumber.getLottoNumbers().contains(LottoNumber.getLottoNumber(1)));
     }
+
     @Test
     void 로또_번호가_1미만일_경우_예외가_발생해야_한다() {
-        Assertions.assertThrows(LottoNumberException.class, ()-> LottoNumber.getLottoNumber(-1));
+        Assertions.assertThrows(LottoNumberException.class, () -> LottoNumber.getLottoNumber(-1));
     }
 
     @Test
     void 로또_번호가_45초과일_경우_예외가_발생해야_한다() {
-        Assertions.assertThrows(LottoNumberException.class, ()-> LottoNumber.getLottoNumber(46));
+        Assertions.assertThrows(LottoNumberException.class, () -> LottoNumber.getLottoNumber(46));
     }
 }
