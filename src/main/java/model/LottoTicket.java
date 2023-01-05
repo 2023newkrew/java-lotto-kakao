8a79e03978c180c7fe852fbcf99d3634fa527b5d
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-05 13:44:57 +0900

    refactor: 불필요한 변수 및 함수 제거

--- src/main/java/model/LottoTicket.java
+++ src/main/java/model/LottoTicket.java
@@ -1,59 +1,21 @@
 /**
- * 사용자의 로또들을 가지고 있다.. 그뿐이다..
- *
+ * 로또의 리스트를 가지고 있다
+ * 로또 리스트의 내부를 변경할 수 없어야 한다
  */
 package model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LottoTicket {
-    private final List<Lotto> lottoList; //복권들의 리스트를 가지고 있다.
+    private final List<Lotto> lottos;
 
-    private final int count;
-
-    private final int totalPay;
-
-    LottoTicket(List<Lotto> lottoList, int totalPay) {
-        this.lottoList = new ArrayList<>(lottoList); //끊어 줄 것..(인자가 변경되어도 값이 변경될 수 없도록..)
-        this.count = lottoList.size();
-        this.totalPay = totalPay;
+    LottoTicket(List<Lotto> lottos) {
+        this.lottos = new ArrayList<>(lottos);
     }
 
-    //public List<Lotto> getLottoList() { 복사해서 리턴.. ->이렇게 해도.. 값을 변경할 수 있나..?
-    //    return ArrayList<>(lottoList);
-    //}
-
-    public int getCount() {
-        return count;
+    public List<Lotto> getLottoList() {
+        return new ArrayList<>(lottos);
     }
 
-    public int getTotalPay() {
-        return totalPay;
-    }
-/*
-    public long getTotalLotteryAmount(final LottoWinner winNumbers) {
-        long result = 0;
-        for(Lotto lotto : lottoList) {
-            result += lotto.getLotteryAmount((int)lotto.getMatchCount(winNumbers.getNumbers()), lotto.isMatchBonusNumber(winNumbers.getBonusNumber()));
-        }
-        return result;
-    }
-
-    public Map<Integer, Integer> getTotalResult(final LottoWinner winNumbers) {
-        Map<Integer, Integer> result = new HashMap<>();
-
-        for(Lotto lotto : lottoList) {
-            int lotteryResult = lotto.getLotteryResult((int)lotto.getMatchCount(winNumbers.getNumbers()), lotto.isMatchBonusNumber(winNumbers.getBonusNumber()));
-            result.put(lotteryResult, result.getOrDefault(lotteryResult, 0) + 1);
-        }
-      return result;
-    }
-
-    public double getTotalLotteryRate(final long amount, final long purchaseAmount) {
-        return (double)amount/purchaseAmount;
-    }
- */
 }
