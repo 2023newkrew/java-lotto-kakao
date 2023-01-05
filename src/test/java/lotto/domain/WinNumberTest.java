061dbf950555729ca23608e2abc531891d8e00fd
Minsu Kim <ravor8878@gmail.com>
2023-01-05 14:40:21 +0900

    refactor : LottoTrial 추상 클래스를 빌더로 변경
    비록 겹치는 메서드가 많기는 하였으나, 상속보다는 빌더로 구현하는 것이 훨씬 낫다고 판단하여 리펙토링하였음.

--- src/test/java/lotto/domain/WinNumberTest.java
+++ src/test/java/lotto/domain/WinNumberTest.java
@@ -13,24 +13,30 @@ class WinNumberTest {
     @Test
     void 보너스_숫자가_원래_숫자와_겹치면_예외(){
         assertThatThrownBy(()-> {
-            WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), LottoBallNumber.get(6));
+            WinNumber winNumber = new WinNumber(
+                    new LottoTrial.Builder().addBalls(List.of(1,2,3,4,5,6)).build(), LottoBallNumber.get(6));
         }).isInstanceOf(DuplicatedBallNumber.class);
     }
     @Test
     void 보너스_숫자가_원래_숫자와_안겹치면_정상실행(){
         assertThatCode(()-> {
-            WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), LottoBallNumber.get(7));
+            WinNumber winNumber = new WinNumber(
+                    new LottoTrial.Builder().addBalls(List.of(1,2,3,4,5,6)).build(), LottoBallNumber.get(7));
         }).doesNotThrowAnyException();
     }
 
     @Test
     void trial에_대한_결과를_잘_반환하는가(){
-        WinNumber winNumber = new WinNumber(new LottoTrialManual(List.of(1,2,3,4,5,6)), LottoBallNumber.get(7));
+        WinNumber winNumber = new WinNumber(
+                new LottoTrial.Builder().addBalls(List.of(1,2,3,4,5,6)).build(),
+                LottoBallNumber.get(7));
 
-        Assertions.assertThat(winNumber.compareLotto(new LottoTrialManual(List.of(1,2,3,4,5,7))))
+        Assertions.assertThat(winNumber.compareLotto(
+                new LottoTrial.Builder().addBalls(List.of(1,2,3,4,5,7)).build()))
                 .isEqualTo(LottoResult.get(5,true));
 
-        Assertions.assertThat(winNumber.compareLotto(new LottoTrialManual(List.of(4,2,3,1,8,9))))
+        Assertions.assertThat(winNumber.compareLotto(
+                new LottoTrial.Builder().addBalls(List.of(4,2,3,1,8,9)).build()))
                 .isEqualTo(LottoResult.get(4,false));
     }
 }
