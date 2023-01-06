5764f537385c7133f1af5b4893c6ab8ffd2b3184
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-06 15:12:36 +0900

    refactor: LottoPrizeMoney 삭제 및 코드 수정

--- src/main/java/model/Banker.java
+++ src/main/java/model/Banker.java
@@ -5,7 +5,6 @@
 package model;
 
 import model.constant.LottoPlace;
-import model.constant.LottoPrizeMoney;
 
 public class Banker {
     public long getTotalPrizeMoney(LottoResult lottoResult) { //전체 상금 리턴
@@ -17,11 +16,6 @@ public class Banker {
     }
 
     private long getPrizeMoney(LottoPlace lottoPlace) { //등수 당 상금 매핑
-        if (lottoPlace == LottoPlace.FIRST_PLACE) return LottoPrizeMoney.FIRST_PRIZE_MONEY.valueOf();
-        if (lottoPlace == LottoPlace.SECOND_PLACE) return LottoPrizeMoney.SECOND_PRIZE_MONEY.valueOf();
-        if (lottoPlace == LottoPlace.THIRD_PLACE) return LottoPrizeMoney.THIRD_PRIZE_MONEY.valueOf();
-        if (lottoPlace == LottoPlace.FOURTH_PLACE) return LottoPrizeMoney.FOURTH_PRIZE_MONEY.valueOf();
-        if (lottoPlace == LottoPlace.FIFTH_PLACE) return LottoPrizeMoney.FIFTH_PRIZE_MONEY.valueOf();
-        return LottoPrizeMoney.LOSE_PRIZE_MONEY.valueOf();
+        return lottoPlace.valueOf();
     }
 }
