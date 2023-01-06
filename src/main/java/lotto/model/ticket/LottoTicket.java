8a2121bd2a292b0d8311e57979bee021f2ece2a8
Eithea <oguraff94@gmail.com>
2023-01-06 16:21:21 +0900

    chore : remove incorrect merged files

--- src/main/java/lotto/model/ticket/LottoTicket.java
+++ src/main/java/lotto/model/ticket/LottoTicket.java
@@ -47,6 +47,6 @@ public class LottoTicket {
         Collections.sort(numbers);
         return "[" + String.join(", ", numbers.stream()
                 .map(String::valueOf)
-                .collect(Collectors.joining())) + "]";
+                .collect(Collectors.toList())) + "]";
     }
 }
