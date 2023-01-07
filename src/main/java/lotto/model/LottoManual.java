8d1c0887ef8a908a3aa3f2daed3d254b514108f0
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-08 02:08:17 +0900

    feat : LottoManual 클래스 생성자 매개변수명 balls로 변경

--- src/main/java/lotto/model/LottoManual.java
+++ src/main/java/lotto/model/LottoManual.java
@@ -4,15 +4,15 @@ import java.util.Collection;
 import java.util.List;
 
 public class LottoManual extends Lotto {
-    public LottoManual(Collection<LottoBall> ballNumbers) {
-        super.balls.addAll(ballNumbers);
+    public LottoManual(Collection<LottoBall> balls) {
+        super.balls.addAll(balls);
 
         super.check();
         super.sort();
     }
 
-    public LottoManual(List<Integer> ballNumbers) {
-        for (int ball : ballNumbers){
+    public LottoManual(List<Integer> balls) {
+        for (int ball : balls){
             super.balls.add(new LottoBall(ball));
         }
 
