df74da6ce8145ae576d9c28063d8787c52173a9d
Ethan <rlfalsgh95@naver.com>
2023-01-08 03:42:58 +0900

    feat : Wallet Test 추가

---
+++ src/test/java/domain/WalletTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class WalletTest {
+    @DisplayName("지갑은 양의 정수의 돈을 가질 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -100})
+    void test1(int value){
+        Assertions.assertThatThrownBy(() -> new Wallet(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지갑에 들어있는 돈 이상의 돈을 사용할 수 없다.")
+    @Test
+    void test2(){
+        Wallet wallet = new Wallet(1000);
+        Assertions.assertThatThrownBy(() -> wallet.use(1001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("음수의 돈을 사용할 수 없다.")
+    @Test
+    void test3(){
+        Wallet wallet = new Wallet(1000);
+        Assertions.assertThatThrownBy(() -> wallet.use(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
