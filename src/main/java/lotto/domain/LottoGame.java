dbbb7fec6bec76f42746e35026d2d4ff93ae95c1
ekzm8523 <mjw8523@gmail.com>
2023-01-08 20:02:02 +0900

    feat: LottoGame 클래스 lottos -> Customer로 변경 & 테스트코드

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -2,34 +2,29 @@ package lotto.domain;
 
 import static lotto.LottoConfig.LOTTO_PRICE;
 
-import java.util.Collections;
-import java.util.List;
 import lotto.dto.GameResultDto;
 
 public class LottoGame {
 
-    private final List<Lotto> lottos;
+    private final Customer customer;
     private final WinningLotto winningLotto;
 
-    public LottoGame(List<Lotto> lottos, WinningLotto winningLotto) {
-        this.lottos = lottos;
+    public LottoGame(Customer customer, WinningLotto winningLotto) {
+        this.customer = customer;
         this.winningLotto = winningLotto;
-    }
-
-    public List<Lotto> getLottos() {
-        return Collections.unmodifiableList(lottos);
     }
 
     public GameResultDto getGameResult() {
         LottoGradeCounter lottoGradeCounter = getLottoGradeCounter();
         float rateOfReturn = (float) lottoGradeCounter.getTotalPrice() /
-                (lottos.size() * LOTTO_PRICE);
+                (customer.getEveryLottoSize() * LOTTO_PRICE);
         return new GameResultDto(lottoGradeCounter.getLottoGradeCountResults(), rateOfReturn);
     }
 
     private LottoGradeCounter getLottoGradeCounter() {
         LottoGradeCounter lottoGradeCounter = new LottoGradeCounter();
-        lottos.forEach(lotto -> lottoGradeCounter.putGrade(winningLotto.getGrade(lotto)));
+        customer.getEveryLottos()
+                .forEach(lotto -> lottoGradeCounter.putGrade(winningLotto.getGrade(lotto)));
         return lottoGradeCounter;
     }
 
--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -6,31 +6,39 @@ import java.util.List;
 import java.util.stream.Stream;
 import lotto.LottoConfig;
 import lotto.LottoGradeEnum;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoGameTest {
 
-    @Test
-    void 로또갯수만큼_로또가_생성된다() {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        LottoGame lottoGame = new LottoGame(
-                lottoGenerator.generateLottos(30000),
-                new WinningLotto(List.of(1,2,3,4,5,6), 7)
-        );
-        assertThat(lottoGame.getLottos().size()).isEqualTo(30);
+    private Customer customer;
+    private WinningLotto winningLotto;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer(10000);
+        winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
     }
+
+    @AfterEach
+    void tearDown() {
+        customer = null;
+        winningLotto = null;
+    }
+
 
     @ParameterizedTest
     @MethodSource("decideLottoArgument")
-    void 수익률을_계산할_수_있다(LottoGame lottoGame, float expected) {
+    void 수익률을_계산할_수_있다(List<Lotto> lottos, float expected) {
+        customer.registerAutoLottos(lottos);
+        LottoGame lottoGame = new LottoGame(customer, winningLotto);
         assertThat(lottoGame.getGameResult().getRateOfReturn()).isEqualTo(expected);
     }
 
     static Stream<Arguments> decideLottoArgument() {
-        WinningLotto WINNING_LOTTO = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto FIRST_GRADE_LOTTO = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Lotto SECOND_GRADE_LOTTO = new Lotto(List.of(1, 2, 3, 4, 5, 7));
         Lotto THIRD_GRADE_LOTTO = new Lotto(List.of(1, 2, 3, 4, 5, 8));
@@ -38,41 +46,28 @@ class LottoGameTest {
         Lotto FIFTH_GRADE_LOTTO = new Lotto(List.of(1, 2, 3, 10, 9, 8));
         return Stream.of(
                 Arguments.arguments(
-                        new LottoGame(
-                                List.of(FIRST_GRADE_LOTTO, FIRST_GRADE_LOTTO),
-                                WINNING_LOTTO
-                        ),
+                        List.of(FIRST_GRADE_LOTTO, FIRST_GRADE_LOTTO),
                         LottoGradeEnum.FIRST.getPrice() / LottoConfig.LOTTO_PRICE
                 ),
                 Arguments.arguments(
-                        new LottoGame(
-                                List.of(SECOND_GRADE_LOTTO, SECOND_GRADE_LOTTO),
-                                WINNING_LOTTO
-                        ),
+                        List.of(SECOND_GRADE_LOTTO, SECOND_GRADE_LOTTO),
                         LottoGradeEnum.SECOND.getPrice() / LottoConfig.LOTTO_PRICE
                 ),
                 Arguments.arguments(
-                        new LottoGame(
-                                List.of(THIRD_GRADE_LOTTO, THIRD_GRADE_LOTTO),
-                                WINNING_LOTTO
-                        ),
+                        List.of(THIRD_GRADE_LOTTO, THIRD_GRADE_LOTTO),
                         LottoGradeEnum.THIRD.getPrice() / LottoConfig.LOTTO_PRICE
                 ),
                 Arguments.arguments(
-                        new LottoGame(
-                                List.of(FOURTH_GRADE_LOTTO, FOURTH_GRADE_LOTTO),
-                                WINNING_LOTTO
-                        ),
+                        List.of(FOURTH_GRADE_LOTTO, FOURTH_GRADE_LOTTO),
                         LottoGradeEnum.FOURTH.getPrice() / LottoConfig.LOTTO_PRICE
                 ),
                 Arguments.arguments(
-                        new LottoGame(
-                                List.of(FIFTH_GRADE_LOTTO, FIFTH_GRADE_LOTTO),
-                                WINNING_LOTTO
-                        ),
+                        List.of(FIFTH_GRADE_LOTTO, FIFTH_GRADE_LOTTO),
                         LottoGradeEnum.FIFTH.getPrice() / LottoConfig.LOTTO_PRICE
                 )
         );
+
+
     }
 
 }
