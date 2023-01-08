8136b996d728644f87e6913f75aed0ffa1fea2e4
Ethan <rlfalsgh95@naver.com>
2023-01-08 15:03:39 +0900

    feat : LottoGameTest 추가

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -7,25 +7,25 @@ public class LottoGame {
     private final List<LottoTicket> lottoTickets;
     private final WinningLotto winningLotto;
 
-    public LottoGame(User user, WinningLotto lastWinningLotto) {
-        this.lottoTickets = user.getLottoTickets();
-        this.winningLotto = lastWinningLotto;
+    public LottoGame(List<LottoTicket> lottoTickets, WinningLotto winningLotto) {
+        this.lottoTickets = lottoTickets;
+        this.winningLotto = winningLotto;
     }
 
-    public LottoMatchResult match(LottoTicket lottoTicket) {
+    private LottoMatchResult match(LottoTicket lottoTicket, WinningLotto winningLotto) {
         List<LottoNumber> unMatchLottoNumbers = lottoTicket.findUnMatchLottoNumbers(winningLotto.getWinningLottoTicket());
         List<LottoNumber> remainLottoNumbers = unMatchLottoNumbers.stream()
                 .filter(lottoNumber -> (!lottoNumber.equals(winningLotto.getBonusNumber())))
                 .collect(Collectors.toList());
 
-        int matchCount = winningLotto.getWinningLottoTicket().size() - unMatchLottoNumbers.size();
+        int matchCount = lottoTicket.size() - unMatchLottoNumbers.size();
         boolean isBonusNumberMatched = (unMatchLottoNumbers.size() - remainLottoNumbers.size() != 0);
         return new LottoMatchResult(matchCount, isBonusNumberMatched);
     }
 
     public GameResult play(){
         List<LottoMatchResult> lottoMatchResults = lottoTickets.stream()
-                .map(this::match)
+                .map((lottoTicket) -> match(lottoTicket, winningLotto))
                 .collect(Collectors.toList());
 
         return new GameResult(lottoMatchResults);
