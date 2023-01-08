148beede133703dbab1474c74d41712c2c37c043
ottoyoon98 <ottoyoon98@gmail.com>
2023-01-09 00:26:21 +0900

    refactor: LottoValue 삭제 및 LottoTicket 수정. - LottoValue 삭제 - LottoTicket 수정 및 유효값 확인.

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,49 +1,65 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoTicket {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 45;
     public static final int VALUES_COUNT = 6;
-    private final List<LottoValue> lottoValues;
 
-    public LottoTicket(List<LottoValue> lottoValues) {
+    private final List<Integer> lottoValues;
+
+    public LottoTicket(List<Integer> lottoValues) {
+        validateValuesRange(lottoValues);
         validateValuesCount(lottoValues);
         validateDistinction(lottoValues);
         this.lottoValues = new ArrayList<>(lottoValues);
     }
 
-    private void validateValuesCount(List<LottoValue> lottoValues) {
+    private void validateValuesRange(List<Integer> lottoValues) {
+        for (int value : lottoValues) {
+            validateValueRange(value);
+        }
+    }
+
+    private void validateValueRange(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException("LottoValue 는 1~45의 정수 값이어야 한다.");
+        }
+    }
+
+    private void validateValuesCount(List<Integer> lottoValues) {
         if (lottoValues.size() != VALUES_COUNT) {
             throw new IllegalArgumentException("입력된 값의 개수가 일치하지 않습니다.");
         }
     }
 
-    private void validateDistinction(List<LottoValue> lottoValues) {
+    private void validateDistinction(List<Integer> lottoValues) {
         if (lottoValues.stream().distinct().count() != VALUES_COUNT) {
             throw new IllegalArgumentException("값들이 중복되지 않아야 합니다.");
         }
     }
 
-    private List<LottoValue> getSortedLottoValues() {
-        LottoValue lv = new LottoValue();
-        ArrayList<LottoValue> sortedLottoValues = new ArrayList<>(lottoValues);
-        sortedLottoValues.sort(lv);
+    private List<Integer> getSortedLottoValues() {
+        ArrayList<Integer> sortedLottoValues = new ArrayList<>(lottoValues);
+        Collections.sort(sortedLottoValues);
         return sortedLottoValues;
     }
 
-    public boolean contains(LottoValue bonusNumber) {
+    public boolean contains(int bonusNumber) {
         return lottoValues.contains(bonusNumber);
     }
 
-    public List<LottoValue> getLottoValues() {
+    public List<Integer> getLottoValues() {
         return new ArrayList<>(lottoValues);
     }
 
     public String toString() {
         List<String> st = new ArrayList<>();
-        for (LottoValue lv : getSortedLottoValues()) {
-            st.add(Integer.toString(lv.getValue()));
+        for (int value : getSortedLottoValues()) {
+            st.add(Integer.toString(value));
         }
         return "[" + String.join(", ", st) + "]";
     }
--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -10,51 +10,42 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 class LottoTicketTest {
     @Test
     void 여섯개의_다른_숫자를_가진_티켓_생성() {
-        assertThatCode(() -> new LottoTicket(
-                Arrays.asList(
-                        new LottoValue(1),
-                        new LottoValue(2),
-                        new LottoValue(3),
-                        new LottoValue(4),
-                        new LottoValue(5),
-                        new LottoValue(6)
-                ))).doesNotThrowAnyException();
+        assertThatCode(() -> new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6))).doesNotThrowAnyException();
     }
 
     @Test
     void 중복된_숫자가_있는_경우_예외_발생() {
         assertThatCode(() -> new LottoTicket(
-                Arrays.asList(
-                        new LottoValue(1),
-                        new LottoValue(2),
-                        new LottoValue(3),
-                        new LottoValue(4),
-                        new LottoValue(3),
-                        new LottoValue(6)
-                ))).isInstanceOf(IllegalArgumentException.class);
+                Arrays.asList(1, 2, 3, 4, 3, 6))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 개수가_6개가_아니면_예외_발생() {
         assertThatCode(() -> new LottoTicket(
-                Arrays.asList(
-                        new LottoValue(1),
-                        new LottoValue(2),
-                        new LottoValue(3),
-                        new LottoValue(4),
-                        new LottoValue(5)
-                ))).isInstanceOf(IllegalArgumentException.class);
+                Arrays.asList(1, 2, 3, 4, 5))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음수가_있으면_예외_발생() {
+        assertThatCode(() -> new LottoTicket(
+                Arrays.asList(-1, 2, 3, 4, 5))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 범위_밖_값이_있는_경우_예외_발생() {
+        assertThatCode(() -> new LottoTicket(
+                Arrays.asList(0, 2, 3, 4, 5))).isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void 범위_밖_값이_있는_경우_예외_발생2() {
+        assertThatCode(() -> new LottoTicket(
+                Arrays.asList(1, 2, 3, 46, 5))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void toString_에서_정렬된_값_반환() {
-        LottoTicket lt = new LottoTicket(Arrays.asList(
-                new LottoValue(5),
-                new LottoValue(4),
-                new LottoValue(3),
-                new LottoValue(7),
-                new LottoValue(2),
-                new LottoValue(9)));
+        LottoTicket lt = new LottoTicket(Arrays.asList(5, 4, 3, 7, 2, 9));
         assertThat(lt.toString()).isEqualTo("[2, 3, 4, 5, 7, 9]");
     }
 }
