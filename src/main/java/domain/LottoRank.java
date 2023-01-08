7a7d8dbdd6a4794c2b800438b682f6aab25d5af9
Ethan <rlfalsgh95@naver.com>
2023-01-08 15:05:37 +0900

    feat : FOURTH_BONUS, FIFTH_BONUS 추가 및 테스트 코드 추가

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public enum LottoRank {
     DEFAULT(0, false, 0),
     FIFTH(3, false, 5_000),
+    FIFTH_BONUS(3, true, 50_000),
     FOURTH(4, false, 50_000),
+    FOURTH_BONUS(4, true, 50_000),
     THIRD(5, false, 1_500_000),
     SECOND(5, true, 30_000_000),
     FIRST(6,false,  2_000_000_000);
