822fa4df220a0e1d015b0b452d6a617e421f956a
Subin Min <tnqls5417@naver.com>
2023-01-09 10:28:16 +0900

    test: Lotto 테스트 추가 및 WinningLotto 테스트 수정
    - getRank 테스트 WinningLotto로 이동
    - Lotto 내 추가된 메서드 테스트 작성
    - Lotto.has(LottoNumber) : 해당 로또번호 포함 여부 반환
    - Lotto.matches(Lotto) : 로또 간 일치하는 로또번호 개수 반환

--- src/test/java/dto/WinningLottoTest.java
+++ src/test/java/dto/WinningLottoTest.java
@@ -2,10 +2,12 @@ package dto;
 
 import domain.Lotto;
 import domain.LottoNumber;
+import domain.LottoRank;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningLottoTest {
@@ -18,5 +20,33 @@ public class WinningLottoTest {
         // when, then
         assertThatThrownBy(() -> new WinningLotto((lotto), bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨로또는_로또의_당첨등수를_알_수_있다_1등() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = Lotto.ofManual(numbers);
+        WinningLotto winningLotto = new WinningLotto(Lotto.ofManual(numbers), new LottoNumber(7));
+
+        // when
+        LottoRank place = winningLotto.getRank(lotto);
+
+        // then
+        assertThat(place).isEqualTo(LottoRank.FIRST_RANK);
+    }
+
+    @Test
+    void 당첨로또는_로또의_당첨등수를_알_수_있다_2등() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 11);
+        Lotto lotto = Lotto.ofManual(List.of(1, 2, 3, 4, 5, 7));
+        WinningLotto winningLotto = new WinningLotto(Lotto.ofManual(numbers), new LottoNumber(7));
+
+        // when
+        LottoRank place = winningLotto.getRank(lotto);
+
+        // then
+        assertThat(place).isEqualTo(LottoRank.SECOND_RANK);
     }
 }
