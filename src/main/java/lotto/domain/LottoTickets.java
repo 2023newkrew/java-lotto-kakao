4909d4106e86205a608a7f4d707c55d297219ba8
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-09 19:26:52 +0900

    refactor(lotto): lottoTickets 클래스 분리

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,33 +1,14 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoTickets {
     private final List<LottoTicket> ticket;
-    private final LottoRandom lottoRandom;
-    private final int purchaseCount;
 
-    public LottoTickets(int amount, List<LottoTicket> manual) {
-        // 입력한 금액이 1000원 미만이면 로또 구입 불가능
-        if(!checkAmountUpperThan1000(amount)){
-            throw new IllegalArgumentException("입력한 금액이 1000원 미만입니다.");
-        }
-
-        this.purchaseCount = amount / 1000;
-        this.lottoRandom = new LottoRandom();
-        this.ticket = IntStream.range(0, purchaseCount)
-                .mapToObj(i -> new LottoTicket(lottoRandom.createRandomNumbers()))
-                .collect(Collectors.toCollection(() -> new ArrayList<>(purchaseCount)));
+    public LottoTickets(List<LottoTicket> auto, List<LottoTicket> manual) {
+        this.ticket = auto;
         this.ticket.addAll(manual);
-    }
-
-    // 사용자가 입력한 금액이 1000원 이상인지 확인
-    private boolean checkAmountUpperThan1000(int amount){
-        return amount >= 1000;
     }
 
     public int getLottoTicketSize(){
@@ -36,9 +17,5 @@ public class LottoTickets {
 
     public List<LottoTicket> getTicket(){
         return Collections.unmodifiableList(this.ticket);
-    }
-
-    public int getPurchaseCount() {
-        return this.purchaseCount;
     }
 }
