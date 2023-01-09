cb51d6f2bc04dfef0d07d58bc12ac0569754b3cf
mkom76 <67891360+mkom76@users.noreply.github.com>
2023-01-09 16:06:11 +0900

    [asher.an] Lotto Step2 (#104)
    * feat : add null,empty input validation
    
    * feat : manual lotto number user input
    
    * feat : manual lotto user input
    
    * refactor : separate user input validation logic from InputView to UserInputValidation util class
    
    * refactor : output format
    
    * refactor : README format
    
    * refactor : immutable return for user input
    
    * refactor : add validation logic(negative input) in lottos

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 /**
  * 구매한 로또 객체들을 관리하는 클래스입니다.
  */
@@ -13,12 +14,29 @@ public class Lottos {
 
     private final List<Lotto> lottos = new ArrayList<>();
 
-    public Lottos(int money) {
-        int numberOfLottos = money / Constants.PRICE_OF_LOTTO;
-        addRandom(numberOfLottos);
+    public Lottos(int money, List<List<Integer>> manualLottos) {
+        int numberOfManualLottos = manualLottos.size();
+        int numberOfAutomatedLottos = money / Constants.PRICE_OF_LOTTO - numberOfManualLottos;
+
+        checkNegative(numberOfAutomatedLottos);
+
+        addManual(manualLottos);
+        addAutomated(numberOfAutomatedLottos);
     }
 
-    private void addRandom(int numberOfLottos) {
+    private void checkNegative(int numberOfAutomatedLottos){
+        if (numberOfAutomatedLottos < 0){
+            throw new IllegalArgumentException("로또 개수는 음수일 수 없습니다.");
+        }
+    }
+
+    private void addManual(List<List<Integer>> manualLottos) {
+        for (List<Integer> manualLotto : manualLottos) {
+            lottos.add(new Lotto(manualLotto));
+        }
+    }
+
+    private void addAutomated(int numberOfLottos) {
         for (int i = 0; i < numberOfLottos; i++) {
             lottos.add(new Lotto());
         }
