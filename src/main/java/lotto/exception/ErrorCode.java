1a8628e1704f2bca224a67a17ee3345c5b0bf895
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-09 11:17:15 +0900

    test: 중복되는 번호가 입력될 시 예외처리 테스트 작성

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
     INVALID_PURCHASE_AMOUNT("구매 금액은 0원 보다 커야합니다."),
     LACK_OF_MONEY("로또를 사기 위한 돈이 부족합니다."),
     INVALID_INPUT_TYPE_NOT_INTEGER("입력 타입이 잘못되었습니다. 숫자를 입력하십시오."),
-    INVALID_MANUAL_LOTTO_COUNT("수동 구매 로또 수가 유효하지 않습니다.");
+    INVALID_MANUAL_LOTTO_COUNT("수동 구매 로또 수가 유효하지 않습니다."),
+    DUPLICATE_LOTTO_NUMBERS("중복된 로또 번호가 포함되어 있습니다.");
 
     private final String message;
 
