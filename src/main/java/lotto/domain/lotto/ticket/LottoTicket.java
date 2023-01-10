0a53d28d11ea914a1b02a35da678055b5a902cfa
jungin0507 <a01033046758@gmail.com>
2023-01-10 18:09:11 +0900

    refactor: 유효성 검사를 먼저 수행하도록 변경

--- src/main/java/lotto/domain/lotto/ticket/LottoTicket.java
+++ src/main/java/lotto/domain/lotto/ticket/LottoTicket.java
@@ -14,23 +14,23 @@ public class LottoTicket {
     private final List<LottoNumber> lottoNumbers;
 
     public LottoTicket(List<LottoNumber> lottoNumbers) {
+        validateSize(lottoNumbers);
+        validateDuplicates(lottoNumbers);
         this.lottoNumbers = new ArrayList<>(lottoNumbers);
         Collections.sort(this.lottoNumbers);
-        validateSize();
-        validateDuplicates();
     }
 
     public List<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
     }
 
-    private void validateSize() {
+    private void validateSize(List<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
 
-    private void validateDuplicates() {
+    private void validateDuplicates(List<LottoNumber> lottoNumbers) {
         if (ListUtils.hasDuplicates(lottoNumbers)) {
             throw new IllegalArgumentException("중복된 로또 번호가 있습니다.");
         }
