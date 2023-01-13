6657de8e6bb0d1ae4703d3e1a1b00def79e40eff
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-13 11:40:12 +0900

    refact: 주석 추가 및 일부 함수 제거

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -1,43 +1,56 @@
 package utils;
 
-import exception.ManualLottoCountException;
+import exception.*;
 import model.Lotto;
 import model.LottoNumber;
 import model.PurchaseMoney;
 
 import java.util.Arrays;
-import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Parser {
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
-    public static Lotto parsingStringToLotto(final String input) throws IllegalArgumentException {
-        Set<LottoNumber> lottoNumberList = Arrays.stream(input.replace(" ", "")
-                        .split(","))
-                .map(Integer::parseInt)
-                .map(LottoNumber::getLottoNumber)
-                .collect(Collectors.toSet());
-        return new Lotto(lottoNumberList);
+    /**
+     * @throws LottoNumberDuplicateException - 입력된 로또 번호에 중복이 있을 경우 발생하는 예외
+     */
+    public static Lotto parsingStringToLotto(final String input) throws LottoNumberDuplicateException {
+        return new Lotto(new HashSet<>(parsingStringToSixNumbers(input)));
     }
 
-    public static LottoNumber parsingStringToLottoNumber(final String input) throws IllegalArgumentException {
-        return LottoNumber.getLottoNumber(Integer.parseInt(input));
+    /**
+     * @throws LottoNumberCountException - 입력된 로또 번호가 6개가 아닐 경우 발생하는 예외
+     */
+    private static List<LottoNumber> parsingStringToSixNumbers(final String input) throws LottoNumberCountException {
+        List<String> inputString = Arrays.stream(input.replace(" ", "").split(",")).collect(Collectors.toList());
+        if (inputString.size() != LOTTO_NUMBER_COUNT) {
+            throw new LottoNumberCountException();
+        }
+        return inputString.stream().map(Parser::parsingStringToLottoNumber).collect(Collectors.toList());
     }
 
-    public static PurchaseMoney parsingStringToPurchaseMoney(final String input) throws IllegalArgumentException {
+    /**
+     * @throws NumberFormatException - 입력이 숫자가 아닐 경우 발생하는 예외
+     * @throws LottoNumberRangeException - 입력된 로또 번호가 1~45 사이의 값이 아닐경우 발생하는 예외
+     */
+    public static LottoNumber parsingStringToLottoNumber(final String input) throws NumberFormatException, LottoNumberRangeException{
+        return LottoNumber.getLottoNumber(parsingStringToInt(input));
+    }
+
+    /**
+     * @throws NumberFormatException - 입력이 숫자가 아닐 경우 발생하는 예외
+     * @throws PurchaseMoneyException - 로또 구입 금액이 1000 미만이거나 1000의 배수가 아닐 경우 발생하는 예외
+     */
+    public static PurchaseMoney parsingStringToPurchaseMoney(final String input) throws NumberFormatException, PurchaseMoneyException{
         return new PurchaseMoney(Long.parseLong(input));
     }
 
-    public static int parsingManualLottoCount(final String input, int totalLottoCount) {
-        int manualLottoCount = 0;
-        try {
-            manualLottoCount = Integer.parseInt(input);
-        } catch (Exception e) {
-            throw (e);
-        }
-        if (manualLottoCount > totalLottoCount || manualLottoCount < 0) { //여기도 감싸서 수정할 것
-            throw new ManualLottoCountException();
-        }
-        return manualLottoCount;
+    /**
+     * @throws NumberFormatException - 입력이 숫자가 아닐 경우 발생하는 예외
+     */
+    public static Integer parsingStringToInt(final String input) throws NumberFormatException {
+        return Integer.parseInt(input);
     }
 }
