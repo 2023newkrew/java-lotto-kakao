6b4c7de27b188251da9180bfbdad2b58ff6afa9d
Ethan <rlfalsgh95@naver.com>
2023-01-13 12:12:43 +0900

    refactor : initGame(), initUserLottoTicket(), initUserAmount() 추출

--- src/main/java/controller/LottoSimulator.java
+++ src/main/java/controller/LottoSimulator.java
@@ -10,40 +10,48 @@ public class LottoSimulator{
     private final InputView inputView;
     private final OutputView outputView;
     private final LottoTicketStore lottoTicketStore;
+    private final User user;
 
     public LottoSimulator(InputView inputView, OutputView outputView, LottoTicketStore lottoTicketStore){
         this.inputView = inputView;
         this.outputView = outputView;
         this.lottoTicketStore = lottoTicketStore;
+        this.user = new User();
     }
 
     public void simulate(){
-        User user = getUserInfo();
+        initGame();
         outputView.printUserInfo(user);
 
         WinningLotto lastWinningLotto = getLastWinningLotto();
 
         LottoGame lottoGame = new LottoGame(user.getLottoTickets(), lastWinningLotto);
         GameResult gameResult = lottoGame.play();
+
         WinningStatistics winningStatistics = new WinningStatistics(gameResult, user.getMoneyUsage());
         outputView.printWinningStatistics(winningStatistics);
     }
 
-    private WinningLotto getLastWinningLotto() {
-        return inputView.getLastWinningLotto();
+    private void initGame() {
+        initUserAmount();
+        initUserLottoTicket();
     }
 
-    private User getUserInfo() {
-        int userAmount = inputView.getUserAmount();
-        User user = new User(userAmount);
-
+    private void initUserLottoTicket() {
         int manualLottoTicketCountToPurchase = inputView.getManualLottoCountToPurchase();
         List<List<LottoNumber>> lottoNumbers = inputView.getManualLottoNumbers(manualLottoTicketCountToPurchase);
         user.buyManualLottoTicket(lottoTicketStore, lottoNumbers, manualLottoTicketCountToPurchase);
 
         int autoLottoTicketCountToPurchase = (user.getRemainAmount()) / LottoTicketStore.AUTO_LOTTO_TICKET_COST;
         user.buyAutoLottoTicket(lottoTicketStore, autoLottoTicketCountToPurchase);
+    }
 
-        return user;
+    private void initUserAmount() {
+        int userAmount = inputView.getUserAmount();
+        user.receiveMoney(userAmount);
+    }
+
+    private WinningLotto getLastWinningLotto() {
+        return inputView.getLastWinningLotto();
     }
 }
