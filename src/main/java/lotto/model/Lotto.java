24c0957801022ff475c53d4b2592424266360841
jade.xyz <jade.xyz@kakaocorp.com>
2023-01-18 16:41:58 +0900

    fix(Lotto): 로또 숫자 내 중복 존재 시 예외 발생
    - 수동으로 입력된 로또 숫자 내에 중복이 존재할 시 예외 발생

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Lotto {
 
     private static final String ERR_LOTTO_NUMBERS_ARE_NOT_SIX = "로또 번호는 6개의 숫자여야 합니다.";
+    private static final String ERR_LOTTO_NUMBERS_ARE_UNIQUE = "로또 번호는 서로 다른 숫자로 이루어져야 합니다.";
+    private static final int LOTTO_NUMBER_SIZE = 6;
 
     protected final List<LottoNumber> numbers;
 
@@ -13,8 +15,11 @@ public class Lotto {
     }
 
     public Lotto(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(ERR_LOTTO_NUMBERS_ARE_NOT_SIX);
+        }
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(ERR_LOTTO_NUMBERS_ARE_UNIQUE);
         }
         this.numbers = LottoNumber.of(numbers);
     }
