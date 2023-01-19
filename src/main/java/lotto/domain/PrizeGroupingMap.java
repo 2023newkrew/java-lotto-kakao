5669f48fd95c6ad03a1f255e1e89b01ae277e797
Sangyups <feels4637@gmail.com>
2023-01-19 14:27:48 +0900

    fix: prizeGroupingMap 이 getProfit() 메소드를 public으로 갖도록 변경 (View와의 분리를 위해)

--- src/main/java/lotto/domain/PrizeGroupingMap.java
+++ src/main/java/lotto/domain/PrizeGroupingMap.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import lotto.domain.constant.LottoRule;
 
 public class PrizeGroupingMap {
@@ -15,7 +14,7 @@ public class PrizeGroupingMap {
         this.prizeGroupingMap = new HashMap<>(prizeGroupingMap);
     }
 
-    private double getProfit() {
+    public double getProfit() {
         double totalSpentMoney = getLottoCount() * LottoRule.PRICE;
         return getTotalPrizeMoney() / totalSpentMoney;
     }
@@ -32,37 +31,5 @@ public class PrizeGroupingMap {
                         e.getPrizeMoney() * this.prizeGroupingMap.getOrDefault(e, List.of()).size()
                 )
                 .sum();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder message = new StringBuilder();
-
-        Arrays.stream(LottoPrize.values())
-                .forEachOrdered((e) ->
-                        message.append(String.format("%s (%d원) - %d개\n",
-                                e.getDescription(), e.getPrizeMoney(),
-                                prizeGroupingMap.getOrDefault(e, List.of()).size()))
-                );
-        message.append(String.format("총 수익률은 %.2f입니다.\n", getProfit()));
-
-        return message.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        PrizeGroupingMap that = (PrizeGroupingMap) o;
-        return Objects.equals(prizeGroupingMap, that.prizeGroupingMap);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(prizeGroupingMap);
     }
 }
--- src/test/java/lotto/domain/PrizeGroupingMapTest.java
+++ src/test/java/lotto/domain/PrizeGroupingMapTest.java
@@ -4,26 +4,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashMap;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PrizeGroupingMapTest {
+
+    private final NumberList numbers = new NumberList(List.of(1, 2, 3, 4, 5, 6));
+
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = numbers.toLotto();
+    }
 
     @Test
     void 로또의_당첨_정보를_넣으면_결과_정보를_반환한다() {
         PrizeGroupingMap prizeGroupingMap = new PrizeGroupingMap(
                 new HashMap<>() {{
-                    put(LottoPrize.FIFTH_PRIZE, List.of());
-                    put(LottoPrize.NONE, List.of());
+                    put(LottoPrize.FIFTH_PRIZE, List.of(lotto));
+                    put(LottoPrize.NONE, List.of(lotto, lotto, lotto, lotto));
                 }}
         );
-        String expected = "6개 일치 (2000000000원) - 0개\n"
-                + "5개 일치, 보너스 볼 일치 (30000000원) - 0개\n"
-                + "5개 일치 (1500000원) - 0개\n"
-                + "4개 일치 (50000원) - 0개\n"
-                + "3개 일치 (5000원) - 1개\n"
-                + "0개 일치 (0원) - 13개\n"
-                + "총 수익률은 0.36입니다.\n";
 
-        assertThat(prizeGroupingMap.toString()).isEqualTo(expected);
+        assertThat(prizeGroupingMap.getProfit()).isEqualTo(1);
     }
 }
